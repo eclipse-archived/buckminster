16491a4c8adf929e76438368554f17c909e2029a
Karel Brezina <karel.brezina@cloudsmith.com>
2007-03-07 09:00:09 +0000

    class PropertyDialog was moved from ..buckminster.ui.editor to buckminster.ui.dialogs. The class was rewritten to extend org.eclipse.jface.Dialog instead of org.eclipse.swt.Dialog.

---
+++ org.eclipse.buckminster.ui/src/java/org/eclipse/buckminster/ui/dialogs/PropertyDialog.java
@@ -0,0 +1,167 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2007, Cloudsmith Inc.
+ * The code, documentation and other materials contained herein have been
+ * licensed under the Eclipse Public License - v 1.0 by the copyright holder
+ * listed above, as the Initial Contributor under such license. The text of
+ * such license is available at www.eclipse.org.
+ ******************************************************************************/
+
+package org.eclipse.buckminster.ui.dialogs;
+
+import org.eclipse.buckminster.ui.UiUtils;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * @author Karel Brezina
+ *
+ */
+public class PropertyDialog extends Dialog
+{
+
+	private Property m_property = null;
+
+	private boolean m_newProperty = false;
+	
+	private Text m_keyText;
+
+	private Text m_valueText;
+
+	private Button m_okButton;
+
+	/**
+	 * DumbMessageDialog constructor
+	 *
+	 * @param parent the parent shell
+	 */
+	public PropertyDialog(Shell parent, Property property)
+	{
+		super(parent);
+		
+		m_property = property;
+		m_newProperty = property == null;
+	}
+
+	@Override
+	protected void configureShell(Shell newShell)
+	{
+		super.configureShell(newShell);
+		
+		if(m_newProperty)
+		{
+			newShell.setText("New Property");
+		}
+		else
+		{
+			newShell.setText("Edit Property");
+		}
+	}
+
+	/**
+	 * Creates the dialog area
+	 *
+	 * @param parent the parent composite
+	 * @return Control
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent)
+	{
+
+		Composite textComposite = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout(2, false);
+		layout.marginTop = 7;
+		layout.marginWidth = 10;
+		textComposite.setLayout(layout);
+		textComposite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+		Label label = UiUtils.createGridLabel(textComposite, "Key:", 1, 0, SWT.NONE);
+		m_keyText = UiUtils.createGridText(textComposite, 1, 0, null, SWT.NONE);
+		GridData data = (GridData)m_keyText.getLayoutData();
+		data.widthHint = 300;
+		m_keyText.setLayoutData(data);
+
+		if(!m_newProperty)
+		{
+			label.setEnabled(false);
+			m_keyText.setEnabled(false);
+		}
+
+		UiUtils.createGridLabel(textComposite, "Value:", 1, 0, SWT.NONE);
+		m_valueText = UiUtils.createGridText(textComposite, 1, 0, null, SWT.NONE);
+
+		m_keyText.setText(m_property == null ? "" : m_property.getKey());
+		m_valueText.setText(m_property == null	? "" : m_property.getValue());
+
+		m_keyText.addModifyListener(new ModifyListener()
+		{
+
+			public void modifyText(ModifyEvent e)
+			{
+				String key = m_keyText.getText();
+
+				if(key != null && key.length() > 0)
+				{
+					m_okButton.setEnabled(true);
+				}
+				else
+				{
+					m_okButton.setEnabled(false);
+				}
+			}
+		});
+		
+		return textComposite;
+	}
+
+	/**
+	 * Creates the buttons
+	 *
+	 * @param parent the parent composite
+	 */
+	@Override
+	protected void createButtonsForButtonBar(Composite parent)
+	{
+		m_okButton = createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
+		m_keyText.notifyListeners(SWT.Modify, new Event());
+	}
+
+	/**
+	 * Handles a button press
+	 *
+	 * @param buttonId the ID of the pressed button
+	 */
+	@Override
+	protected void buttonPressed(int buttonId)
+	{
+		// If they press I Dunno, close the dialog
+		if(buttonId == IDialogConstants.CANCEL_ID)
+		{
+			m_property = null;
+		}
+		else
+		{
+			m_property = new Property(m_keyText.getText(), m_valueText.getText());
+		}
+		
+		setReturnCode(buttonId);
+		close();		
+	}
+	
+	public Property getProperty()
+	{
+		return m_property;
+	}
+}
