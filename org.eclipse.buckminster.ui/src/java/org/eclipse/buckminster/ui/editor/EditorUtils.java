0ee4b50e7f3b32f96f6ba674a9b521cf237ea7e9
Karel Brezina <karel.brezina@cloudsmith.com>
2007-05-24 17:29:08 +0000

    Initial import

---
+++ org.eclipse.buckminster.ui/src/java/org/eclipse/buckminster/ui/editor/EditorUtils.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2007, Cloudsmith Inc.
+ * The code, documentation and other materials contained herein have been
+ * licensed under the Eclipse Public License - v 1.0 by the copyright holder
+ * listed above, as the Initial Contributor under such license. The text of
+ * such license is available at www.eclipse.org.
+ ******************************************************************************/
+
+package org.eclipse.buckminster.ui.editor;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+/**
+ * @author Karel Brezina
+ *
+ */
+public class EditorUtils
+{
+	private EditorUtils()
+	{
+	}
+	
+	public static Composite getNamedTabComposite(Composite parent, String header)
+	{
+		Composite tabComposite = new Composite(parent, SWT.NONE);
+		tabComposite.setLayout(new GridLayout(1, true));
+		tabComposite.setBackground(parent.getDisplay().getSystemColor(SWT.COLOR_WHITE));
+		tabComposite.setBackgroundMode(SWT.INHERIT_FORCE);
+
+		Label headerLabel = new Label(tabComposite, SWT.BOLD);
+		headerLabel.setText(header);
+		headerLabel.setForeground(parent.getDisplay().getSystemColor(SWT.COLOR_BLUE));
+		FontData fontData = new FontData();
+		fontData.setHeight(14);
+		headerLabel.setFont(new Font(tabComposite.getDisplay(), fontData));
+		GridData gridData = new GridData(SWT.FILL, SWT.FILL, false, false);
+		gridData.heightHint = 30;
+		headerLabel.setLayoutData(gridData);
+
+		return tabComposite;
+	}
+}
