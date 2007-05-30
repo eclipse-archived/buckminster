3d43424d542edfd28347d67fc21eb9396fb008a9
Karel Brezina <karel.brezina@cloudsmith.com>
2007-05-30 10:23:46 +0000

    Initial import

---
+++ org.eclipse.buckminster.ui/src/java/org/eclipse/buckminster/ui/editor/cspec/ArtifactsTable.java
@@ -0,0 +1,191 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2007, Cloudsmith Inc.
+ * The code, documentation and other materials contained herein have been
+ * licensed under the Eclipse Public License - v 1.0 by the copyright holder
+ * listed above, as the Initial Contributor under such license. The text of
+ * such license is available at www.eclipse.org.
+ ******************************************************************************/
+
+package org.eclipse.buckminster.ui.editor.cspec;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.buckminster.core.cspec.builder.ArtifactBuilder;
+import org.eclipse.buckminster.core.cspec.builder.CSpecBuilder;
+import org.eclipse.buckminster.core.cspec.model.PathAlreadyDefinedException;
+import org.eclipse.buckminster.core.helpers.TextUtils;
+import org.eclipse.buckminster.ui.UiUtils;
+import org.eclipse.buckminster.ui.editor.EditorUtils;
+import org.eclipse.buckminster.ui.general.editor.ValidatorException;
+import org.eclipse.buckminster.ui.general.editor.simple.SimpleTableEditor;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * @author Karel Brezina
+ *
+ */
+public class ArtifactsTable extends AttributesTable<ArtifactBuilder>
+{
+	private Comparator<IPath> s_pathComparator = new Comparator<IPath>()
+	{
+
+		public int compare(IPath o1, IPath o2)
+		{
+			return o1.toOSString().compareTo(o2.toOSString());
+		}
+	};
+	
+	private Text m_basePathText;
+	private Text m_typeText;
+	private List<IPath> m_paths = new ArrayList<IPath>();
+	private SimpleTableEditor<IPath> m_pathsEditor;
+	
+	public ArtifactsTable(List<ArtifactBuilder> data, CSpecBuilder cspec)
+	{
+		super(data, cspec);
+	}
+
+	@Override
+	protected ArtifactBuilder createNewRow()
+	{
+		return getCSpecBuilder().createArtifactBuilder();
+	}
+	
+	@Override
+	protected void setRowValues(ArtifactBuilder builder) throws ValidatorException
+	{
+		super.setRowValues(builder);
+		
+		String basePathString = UiUtils.trimmedValue(m_basePathText);
+		IPath basePath = null;
+		
+		if(basePathString != null)
+		{
+			basePath = Path.fromOSString(basePathString);
+		}
+		builder.setBase(basePath);
+		
+		builder.setType(UiUtils.trimmedValue(m_typeText));
+		
+		Set<IPath> paths = builder.getPaths();
+		
+		if(paths != null)
+		{
+			builder.getPaths().clear();
+		}
+		for(IPath path : m_paths)
+		{
+			try
+			{
+				builder.addPath(path);
+			}
+			catch(PathAlreadyDefinedException e)
+			{
+				throw new ValidatorException(e.getMessage());
+			}
+		}
+	}
+	
+	public void fillStack(Composite stackComposite)
+	{
+		addStackMapping("General", createGeneralStackLayer(stackComposite));
+		addStackMapping("Hints", createInstallerHintsStackLayer(stackComposite));
+		addStackMapping("Paths", createPathsStackLayer(stackComposite));
+		addStackMapping("Documentation", createDocumentationStackLayer(stackComposite));
+	}
+
+	private Control createGeneralStackLayer(Composite stackComposite)
+	{
+		Composite geComposite = new Composite(stackComposite, SWT.NONE);
+		GridLayout layout = new GridLayout(2, false);
+		layout.marginHeight = layout.marginWidth = 0;
+		geComposite.setLayout(layout);
+
+		EditorUtils.createHeaderLabel(geComposite, "General", 2);
+
+		UiUtils.createGridLabel(geComposite, "Name:", 1, 0, SWT.NONE);
+
+		setNameText(UiUtils.createGridText(geComposite, 1, 0, null, SWT.NONE));
+
+		UiUtils.createGridLabel(geComposite, "Public:", 1, 0, SWT.NONE);
+
+		setPublicCheck(UiUtils.createCheckButton(geComposite, null, null));
+		
+		UiUtils.createGridLabel(geComposite, "Base Path:", 1, 0, SWT.NONE);
+
+		m_basePathText = UiUtils.createGridText(geComposite, 1, 0, null, SWT.NONE);
+		
+		UiUtils.createGridLabel(geComposite, "Type:", 1, 0, SWT.NONE);
+
+		m_typeText = UiUtils.createGridText(geComposite, 1, 0, null, SWT.NONE);
+		
+		return geComposite;
+	}
+	
+	private Control createPathsStackLayer(Composite stackComposite)
+	{
+		Composite pthComposite = new Composite(stackComposite, SWT.NONE);
+		GridLayout layout = new GridLayout(1, false);
+		layout.marginHeight = layout.marginWidth = 0;
+		pthComposite.setLayout(layout);
+
+		EditorUtils.createHeaderLabel(pthComposite, "Paths", 1);
+
+		PathsTable phTable = new PathsTable(m_paths);
+		
+		m_pathsEditor = new SimpleTableEditor<IPath>(
+				pthComposite,
+				phTable,
+				null,
+				"Artifact - Path",
+				null,
+				null,
+				SWT.NONE);
+		
+		return pthComposite;
+	}
+	
+	@Override
+	protected void refreshRow(ArtifactBuilder builder)
+	{
+		super.refreshRow(builder);
+		
+		IPath basePath = builder.getBase();
+		m_basePathText.setText(TextUtils.notNullString(basePath ==  null ? null : basePath.toOSString()));
+		m_typeText.setText(TextUtils.notNullString(builder.getType()));
+		
+		m_paths.clear();
+		Set<IPath> paths = builder.getPaths();
+		if(paths != null)
+		{
+			IPath[] pathArray = paths.toArray(new IPath[0]);
+			Arrays.sort(pathArray, s_pathComparator);
+			
+			for(IPath path : pathArray)
+			{
+				m_paths.add(path);
+			}
+		}
+		m_pathsEditor.refresh();
+	}
+
+	@Override
+	public void enableFields(boolean enabled)
+	{
+		super.enableFields(enabled);
+		
+		m_basePathText.setEnabled(enabled);
+		m_typeText.setEnabled(enabled);
+		m_pathsEditor.setEnabled(enabled);
+	}
+}
---
+++ org.eclipse.buckminster.ui/src/java/org/eclipse/buckminster/ui/editor/cspec/AttributesTable.java
@@ -0,0 +1,207 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2007, Cloudsmith Inc.
+ * The code, documentation and other materials contained herein have been
+ * licensed under the Eclipse Public License - v 1.0 by the copyright holder
+ * listed above, as the Initial Contributor under such license. The text of
+ * such license is available at www.eclipse.org.
+ ******************************************************************************/
+
+package org.eclipse.buckminster.ui.editor.cspec;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import org.eclipse.buckminster.core.common.model.Documentation;
+import org.eclipse.buckminster.core.common.model.ExpandingProperties;
+import org.eclipse.buckminster.core.cspec.builder.AttributeBuilder;
+import org.eclipse.buckminster.core.cspec.builder.CSpecBuilder;
+import org.eclipse.buckminster.core.helpers.TextUtils;
+import org.eclipse.buckminster.ui.UiUtils;
+import org.eclipse.buckminster.ui.editor.EditorUtils;
+import org.eclipse.buckminster.ui.general.editor.ValidatorException;
+import org.eclipse.buckminster.ui.general.editor.onepage.OnePageTable;
+import org.eclipse.buckminster.ui.general.editor.simple.SimpleTableEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * @author Karel Brezina
+ *
+ */
+public abstract class AttributesTable<T extends AttributeBuilder> extends OnePageTable<T>
+{
+	private Comparator<Property> s_propertyComparator = new Comparator<Property>()
+	{
+
+		public int compare(Property o1, Property o2)
+		{
+			return o1.getKey().compareTo(o2.getKey());
+		}
+	};
+
+	private CSpecBuilder m_cspec;
+	
+	private Text m_nameText;
+	private Button m_publicCheck;
+	private List<Property> m_installerHints = new ArrayList<Property>();
+	private SimpleTableEditor<Property> m_installerHintsEditor;
+	private Text m_documentationText;
+
+	public AttributesTable(List<T> data, CSpecBuilder cspec)
+	{
+		super(data);
+		m_cspec = cspec;
+	}
+
+	protected CSpecBuilder getCSpecBuilder()
+	{
+		return m_cspec;
+	}
+	
+	protected void setNameText(Text nameText)
+	{
+		m_nameText = nameText;
+	}
+	
+	protected void setPublicCheck(Button publicCheck)
+	{
+		m_publicCheck = publicCheck;
+	}
+	
+	@Override
+	protected void setRowValues(T builder) throws ValidatorException
+	{
+		builder.setName(UiUtils.trimmedValue(m_nameText));	
+		builder.setPublic(m_publicCheck.getSelection());
+		
+		ExpandingProperties hints = builder.getInstallerHints();
+		
+		if(hints != null)
+		{
+			hints.clear();
+		}
+		for(Property property : m_installerHints)
+		{
+			builder.addInstallerHint(property.getKey(), property.getValue());
+		}
+		
+		
+		String doc = UiUtils.trimmedValue(m_documentationText);
+		
+		try
+		{
+			builder.setDocumentation(doc == null ? null : Documentation.parse(doc));
+		}
+		catch(Exception e)
+		{
+			throw new ValidatorException(e.getMessage());
+		}
+	}
+
+	public void enableFields(boolean enabled)
+	{
+		m_nameText.setEnabled(enabled);
+		m_publicCheck.setEnabled(enabled);
+		m_installerHintsEditor.setEnabled(enabled);
+		m_documentationText.setEnabled(enabled);
+	}
+
+	protected Control createInstallerHintsStackLayer(Composite stackComposite)
+	{
+		Composite ihComposite = new Composite(stackComposite, SWT.NONE);
+		GridLayout layout = new GridLayout(1, false);
+		layout.marginHeight = layout.marginWidth = 0;
+		ihComposite.setLayout(layout);
+
+		EditorUtils.createHeaderLabel(ihComposite, "Installer Hints", 1);
+
+		InstallerHintsTable ihTable = new InstallerHintsTable(m_installerHints);
+		
+		m_installerHintsEditor = new SimpleTableEditor<Property>(
+				ihComposite,
+				ihTable,
+				null,
+				"Artifact - Installer Hint",
+				null,
+				null,
+				SWT.NONE);
+
+		return ihComposite;
+	}
+	
+	protected Control createDocumentationStackLayer(Composite stackComposite)
+	{
+		Composite docComposite = new Composite(stackComposite, SWT.NONE);
+		GridLayout layout = new GridLayout(1, false);
+		layout.marginHeight = layout.marginWidth = 0;
+		docComposite.setLayout(layout);
+
+		EditorUtils.createHeaderLabel(docComposite, "Documentation", 1);
+
+		m_documentationText = UiUtils.createGridText(docComposite, 1, 0, null, SWT.MULTI);
+		m_documentationText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+		return docComposite;
+	}
+	
+	public String[] getTableViewerColumnHeaders()
+	{
+		return new String[] {"Name", "Public"};
+	}
+
+	public int[] getTableViewerColumnWeights()
+	{
+		return new int[] {80, 20};
+	}
+
+	public Object getTableViewerField(T builder, int columnIndex)
+	{
+		switch(columnIndex)
+		{
+		case 0:
+			return builder.getName();
+		case 1:
+			return Boolean.valueOf(builder.isPublic());
+		default:
+			return null;
+		}
+	}
+
+	@Override
+	protected void refreshRow(T builder)
+	{
+		m_nameText.setText(TextUtils.notNullString(builder.getName()));
+		m_publicCheck.setSelection(builder.isPublic());
+		
+		m_installerHints.clear();
+		ExpandingProperties properties = builder.getInstallerHints();
+		if(properties != null)
+		{
+			List<Property> hlpList = new ArrayList<Property>();
+			for(String key : properties.keySet())
+			{
+				hlpList.add(new Property(key, properties.get(key)));
+			}
+			Property[] propertyArray = hlpList.toArray(new Property[0]);
+			Arrays.sort(propertyArray, s_propertyComparator);
+			
+			for(Property property : propertyArray)
+			{
+				m_installerHints.add(property);
+			}
+		}
+		m_installerHintsEditor.refresh();
+		
+		
+		Documentation doc = builder.getDocumentation();
+		m_documentationText.setText(TextUtils.notNullString(doc == null ? null : doc.toString()));
+	}
+
+}
