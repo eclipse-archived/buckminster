41810d10ee53e8e5a4534b4a58e81cd0d06a8f03
Filip Hrbek <filip.hrbek@gmail.com>
2007-06-12 16:05:47 +0000

    Improved download speed estimation (checking only a few seconds back instead overall average speed)

--- org.eclipse.buckminster.core/src/java/org/eclipse/buckminster/core/helpers/ProgressStatistics.java
+++ org.eclipse.buckminster.core/src/java/org/eclipse/buckminster/core/helpers/ProgressStatistics.java
@@ -9,11 +9,18 @@
 package org.eclipse.buckminster.core.helpers;
 
 import java.util.Date;
+import java.util.Locale;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class ProgressStatistics
 {
 	public static final AmountConverter TRIVIAL_CONVERTER = new TrivialConverter();
 	public static final AmountConverter FILESIZE_CONVERTER = new FileSizeConverter();
+	
+	public static final int DEFAULT_REPORT_INTERVAL = 1000;
+	public static final int DEFAULT_RECENT_SPEED_INTERVAL = 5000;
+	public static final int DEFULAT_RECENT_SPEED_RESOLUTION = 1000;
 
 	private long m_current;
 	private long m_total;
@@ -21,6 +28,10 @@ public class ProgressStatistics
 	private AmountConverter m_converter;
 	private Date m_lastReportTime;
 	private int m_reportInterval;
+	private int m_recentSpeedInterval;
+	private int m_recentSpeedResolution;
+	private SortedMap<Long, Long> m_recentSpeedMap;
+	private long m_recentSpeedMapKey;
 	
 	public ProgressStatistics()
 	{
@@ -28,8 +39,13 @@ public class ProgressStatistics
 		m_total = -1;
 		m_startTime = new Date();
 		m_lastReportTime = null;
-		m_reportInterval = 1000;
+		m_reportInterval = DEFAULT_REPORT_INTERVAL;
+		m_recentSpeedInterval = DEFAULT_RECENT_SPEED_INTERVAL;
+		m_recentSpeedResolution = DEFULAT_RECENT_SPEED_RESOLUTION;
 		m_converter = TRIVIAL_CONVERTER;
+		
+		m_recentSpeedMap = new TreeMap<Long, Long>();
+		m_recentSpeedMapKey = 0L;
 	}
 
 	public ProgressStatistics(long total)
@@ -48,6 +64,7 @@ public class ProgressStatistics
 
 	public void increase(long inc)
 	{
+		registerRecentSpeed(getDuration() / m_recentSpeedResolution, inc);
 		m_current += inc;
 	}
 	
@@ -66,6 +83,51 @@ public class ProgressStatistics
 		return 0L;
 	}
 	
+	synchronized public long getRecentSpeed()
+	{
+		removeObsoleteRecentSpeedData(getDuration() / m_recentSpeedResolution);
+		long dur = 0L;
+		long amount = 0L;
+		SortedMap<Long, Long> relevantData = m_recentSpeedMap.headMap(Long.valueOf(m_recentSpeedMapKey));
+		
+		for (Long key : relevantData.keySet())
+		{
+			dur += m_recentSpeedResolution;
+			amount += relevantData.get(key).longValue();
+		}
+		
+		if (dur >= 1000)
+			return amount / (dur / 1000);
+
+		return 0L;
+	}
+	
+	public int getRecentSpeedInterval()
+	{
+		return m_recentSpeedInterval;
+	}
+
+	public void setRecentSpeedInterval(int recentSpeedInterval)
+	{
+		if (recentSpeedInterval <= 0)
+			recentSpeedInterval = DEFAULT_RECENT_SPEED_INTERVAL;
+		m_recentSpeedInterval = recentSpeedInterval;
+	}
+
+
+	public int getRecentSpeedResolution()
+	{
+		return m_recentSpeedResolution;
+	}
+
+	public void setRecentSpeedResolution(int recentSpeedResolution)
+	{
+		if (recentSpeedResolution <= 0)
+			recentSpeedResolution = DEFULAT_RECENT_SPEED_RESOLUTION;
+
+		m_recentSpeedResolution = recentSpeedResolution;
+	}
+
 	public double getPercentage()
 	{
 		if (m_total > 0)
@@ -102,7 +164,7 @@ public class ProgressStatistics
 		return
 			m_converter.convert(m_current) +
 			(m_total != -1 ? " of " + m_converter.convert(m_total) : "") + " at " +
-			m_converter.convert(getAverageSpeed()) + "/s"; 
+			m_converter.convert(getRecentSpeed()) + "/s"; 
 	}
 
 	@Override
@@ -111,6 +173,29 @@ public class ProgressStatistics
 		return report(); 
 	}
 	
+	synchronized private void registerRecentSpeed(long key, long inc)
+	{
+		Long keyL = Long.valueOf(key);
+		Long currentValueL = m_recentSpeedMap.get(keyL);
+		long currentValue = 0L;
+		if (currentValueL != null)
+			currentValue = currentValueL.longValue();
+
+		m_recentSpeedMap.put(keyL, Long.valueOf(inc + currentValue));
+		
+		if (m_recentSpeedMapKey != key)
+		{
+			m_recentSpeedMapKey = key;
+			removeObsoleteRecentSpeedData(key);
+		}
+	}
+
+	synchronized private void removeObsoleteRecentSpeedData(long lastKey)
+	{
+		long threshold = lastKey - m_recentSpeedInterval / m_recentSpeedResolution;
+		m_recentSpeedMap.headMap(Long.valueOf(threshold)).clear();
+	}
+
 	public interface AmountConverter
 	{
 		String convert(long amount);
@@ -132,11 +217,11 @@ public class ProgressStatistics
 		public String convert(long amount)
 		{
 			if (amount < 1024)
-				return String.format("%dB", Long.valueOf(amount));
+				return String.format(Locale.US, "%dB", Long.valueOf(amount));
 			else if (amount < 1024*1024)
-				return String.format("%.2fkB", Double.valueOf(((double) amount)/1024));
+				return String.format(Locale.US, "%.2fkB", Double.valueOf(((double) amount)/1024));
 			else
-				return String.format("%.2fMB", Double.valueOf(((double) amount)/(1024*1024)));
+				return String.format(Locale.US, "%.2fMB", Double.valueOf(((double) amount)/(1024*1024)));
 		}
 		
 	}
--- org.eclipse.buckminster.core/src/java/org/eclipse/buckminster/core/helpers/ShortDurationURLCache.java
+++ org.eclipse.buckminster.core/src/java/org/eclipse/buckminster/core/helpers/ShortDurationURLCache.java
@@ -90,18 +90,79 @@ public class ShortDurationURLCache extends ShortDurationFileCache
 					{
 						ProgressStatistics progress = new ProgressStatistics(info.getSize() != null ? info.getSize().longValue() : -1);
 						progress.setConverter(ProgressStatistics.FILESIZE_CONVERTER);
-
-						while((count = input.read(buf)) >= 0)
+						
+						/*
+						 * This class is used for reporting a good progress information even if the download is stalled for a while.
+						 * If specified timeout expires before something is read from the input, a new progress message is generated.
+						 * This enables e.g. reporting recalculated download speed which would be frozen if we simply waited for data.
+						 */
+						class ProgressReporter extends Thread
 						{
-							output.write(buf, 0, count);
-							progress.increase(count);
+							private IProgressMonitor m_reporterMonitor;
+							private ProgressStatistics m_reporterProgress;
+							private String m_format;
+							private int m_timeout;
+							private boolean m_running;
+							private Thread m_reporter;
 							
-							if (progress.shouldReport())
-								writeMonitor.subTask("Fetching " + info.getName() + " (" + progress.report() + ")");
+							public ProgressReporter(IProgressMonitor reporterMonitor, ProgressStatistics reporterProgress, String format, int timeout)
+							{
+								m_reporterMonitor = reporterMonitor;
+								m_reporterProgress = reporterProgress;
+								m_format = format;
+								m_timeout = timeout;
+								m_running = true;
+							}
 
-							MonitorUtils.worked(
-									writeMonitor,
-									info.getSize() != null ? count : 1);
+							@Override
+							public void run()
+							{
+								m_reporter = Thread.currentThread();
+								
+								while (m_running)
+								{
+									try
+									{
+										Thread.sleep(m_timeout);
+									}
+									catch(InterruptedException e)
+									{
+										// ignore, it's ok
+									}
+									
+									if (m_running && m_reporterProgress.shouldReport())
+										m_reporterMonitor.subTask(String.format(m_format, m_reporterProgress.report()));
+								}
+							}
+							
+							public void stopReporting()
+							{
+								m_running = false;
+								m_reporter.interrupt();
+							}
+						}
+
+						ProgressReporter progressReporter = new ProgressReporter(writeMonitor, progress, "Fetching " + info.getName() + " (%s)", progress.getReportInterval());
+						progressReporter.start();
+
+						try
+						{
+							while((count = input.read(buf)) >= 0)
+							{
+								output.write(buf, 0, count);
+								progress.increase(count);
+								
+								// Bump the reporter to report the change
+								progressReporter.interrupt();
+								
+								MonitorUtils.worked(
+										writeMonitor,
+										info.getSize() != null ? count : 1);
+							}
+						}
+						finally
+						{
+							progressReporter.stopReporting();
 						}
 					}
 					finally
