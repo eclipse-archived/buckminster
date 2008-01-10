e3ef8b346d4b64446bac62455ec6b0182dd58227
Karel Brezina <karel.brezina@cloudsmith.com>
2008-01-10 12:06:55 +0000

    Bug #214687 - when the New wizard is used to create a new CSPEC, the component type is default to "buckminster", the version string is "1.0.0" and the version type is "OSGi".

--- org.eclipse.buckminster.ui/src/java/org/eclipse/buckminster/ui/wizards/NewCSPECWizard.java
+++ org.eclipse.buckminster.ui/src/java/org/eclipse/buckminster/ui/wizards/NewCSPECWizard.java
@@ -8,14 +8,23 @@
 package org.eclipse.buckminster.ui.wizards;
 
 import java.io.InputStream;
-import java.io.PrintStream;
 
+import org.eclipse.buckminster.core.cspec.builder.CSpecBuilder;
+import org.eclipse.buckminster.core.cspec.model.CSpec;
 import org.eclipse.buckminster.core.helpers.AccessibleByteArrayOutputStream;
+import org.eclipse.buckminster.sax.Utils;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.ui.INewWizard;
+import org.xml.sax.SAXException;
 
 public class NewCSPECWizard extends NewBMFileWizard implements INewWizard
 {
-
+	private final String INIT_COMPONENT_TYPE = "buckminster";
+	
+	private final String INIT_VERSION_STRING = "1.0.0";
+	
+	private final String INIT_VERSION_TYPE = "OSGi";
+	
 	public NewCSPECWizard()
 	{
 		super();
@@ -32,6 +41,7 @@ public class NewCSPECWizard extends NewBMFileWizard implements INewWizard
 
 	/**
 	 * We will initialize file contents with a sample text.
+	 * @throws SAXException 
 	 */
 
 	@Override
@@ -42,32 +52,31 @@ public class NewCSPECWizard extends NewBMFileWizard implements INewWizard
 		if(lastSlash >= 0)
 			name = name.substring(lastSlash + 1);
 
+		CSpecBuilder builder = new CSpecBuilder();
+		builder.setName(name);
+		builder.setComponentTypeID(INIT_COMPONENT_TYPE);
+		
+		try
+		{
+			builder.setVersion(INIT_VERSION_STRING, INIT_VERSION_TYPE);
+		}
+		catch(CoreException e)
+		{
+			// initial version won't be set
+		}
+			
+		CSpec cspec = new CSpec(builder);
+		
 		AccessibleByteArrayOutputStream bld = new AccessibleByteArrayOutputStream();
-		PrintStream contents = new PrintStream(bld);
-		contents.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-		contents.println("<cspec");
-		contents.println("\txmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"");
-		contents.println("\txmlns:bc=\"http://www.eclipse.org/buckminster/Common-1.0\"");
-		contents.print("\txmlns=\"http://www.eclipse.org/buckminster/CSpec-1.0\" name=\"");
-		contents.print(name);
-		contents.println("\">");
-		contents.println("\t<dependencies>");
-		contents.println("\t\t<!-- Place your Dependencies here -->");
-		contents.println("\t</dependencies>");
-		contents.println("\t<generators>");
-		contents.println("\t\t<!-- Place your Generators here -->");
-		contents.println("\t</generators>");
-		contents.println("\t<artifacts>");
-		contents.println("\t\t<!-- Place your Artifacts here -->");
-		contents.println("\t</artifacts>");
-		contents.println("\t<actions>");
-		contents.println("\t\t<!-- Place your Actions here -->");
-		contents.println("\t</actions>");
-		contents.println("\t<groups>");
-		contents.println("\t\t<!-- Place your Groups here -->");
-		contents.println("\t</groups>");
-		contents.println("</cspec>");
-		contents.flush();
+		try
+		{
+			Utils.serialize(cspec, bld);
+		}
+		catch(SAXException e)
+		{
+			throw new RuntimeException("Cannot create a new Buckminster Component Specification File", e);
+		}
+		
 		return bld.getInputStream();
 	}
 }
