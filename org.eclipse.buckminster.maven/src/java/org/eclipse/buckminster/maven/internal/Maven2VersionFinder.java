7b4ba9f5a0e3ae360ecb5c420eaf0186cfc322f7
Thomas Hallgren <thomas@tada.se>
2008-01-26 13:17:37 +0000

    Applied patch https://bugs.eclipse.org/bugs/show_bug.cgi?id=216659

--- org.eclipse.buckminster.maven/src/java/org/eclipse/buckminster/maven/internal/Maven2VersionFinder.java
+++ org.eclipse.buckminster.maven/src/java/org/eclipse/buckminster/maven/internal/Maven2VersionFinder.java
@@ -10,18 +10,24 @@ package org.eclipse.buckminster.maven.internal;
 import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.eclipse.buckminster.core.CorePlugin;
 import org.eclipse.buckminster.core.ctype.IComponentType;
-import org.eclipse.buckminster.core.reader.URLCatalogReaderType;
 import org.eclipse.buckminster.core.resolver.NodeQuery;
 import org.eclipse.buckminster.core.rmap.model.Provider;
 import org.eclipse.buckminster.core.version.IVersionDesignator;
 import org.eclipse.buckminster.core.version.VersionMatch;
+import org.eclipse.buckminster.runtime.BuckminsterException;
 import org.eclipse.buckminster.runtime.MonitorUtils;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 /**
  * @author Thomas Hallgren
@@ -32,12 +38,6 @@ public class Maven2VersionFinder extends MavenVersionFinder
 	throws CoreException
 	{
 		super(readerType, provider, ctype, query);
-	}
-
-	private void appendFilesInFolder(URL folder, ArrayList<URL> fileList, IProgressMonitor monitor) throws CoreException
-	{
-		for(URL url : URLCatalogReaderType.list(folder, monitor))
-			fileList.add(url);
 	}
 
 	@Override
@@ -56,31 +56,26 @@ public class Maven2VersionFinder extends MavenVersionFinder
 		monitor.beginTask(null, 2000);
 		try
 		{
-			// Add entries from all matching folders
-			//
 			NodeQuery query = getQuery();
-			boolean defaultIsMatched = query.isMatch(null, null, getProvider().getSpace());
-			for(URL versionURL : URLCatalogReaderType.list(readerType.createURL(uri, rootPath), MonitorUtils.subMonitor(monitor, 1000)))
+			for(String version : getVersions(query.getVersionDesignator(), readerType, uri, rootPath, MonitorUtils.subMonitor(monitor, 1000)))
 			{
-				IPath versionPath = new Path(versionURL.getPath());
-				int segCnt = versionPath.segmentCount();
-				if(segCnt < 1)
-					continue;
-
-				String folderName = versionPath.segment(segCnt - 1);
-				if(!defaultIsMatched)
+				VersionMatch versionMatch = MavenComponentType.createVersionMatch(version, space, null);
+				if(versionMatch != null && query.isMatch(versionMatch))
 				{
-					// No use scanning this folder if the version is incompatible with
-					// the query
-					//
-					VersionMatch versionMatch = MavenComponentType.createVersionMatch(folderName, space, null);
-					if(versionMatch == null || !query.isMatch(versionMatch))
-						continue;
-				}
+					pbld.setLength(rootLen);
+					pbld.append(version);
+					pbld.append('/');
 
-				pbld.setLength(rootLen);
-				readerType.appendFolder(pbld, folderName);
-				appendFilesInFolder(versionURL, fileList, MonitorUtils.subMonitor(monitor, 1000));
+					pbld.append(getMapEntry().getArtifactId());
+					pbld.append('-');
+					if (isSnapshotVersion(version))
+						appendSnapshotIdentifier(pbld, readerType, uri, rootPath, version, monitor);
+					else
+						pbld.append(version);
+					pbld.append(".jar");
+
+					fileList.add(readerType.createURL(uri, pbld.toString()));
+				}
 			}
 			return fileList.toArray(new URL[fileList.size()]);
 		}
@@ -88,5 +83,108 @@ public class Maven2VersionFinder extends MavenVersionFinder
 		{
 			monitor.done();
 		}
+	}
+
+	private List<String> getVersions(IVersionDesignator versionDesignator, Maven2ReaderType readerType, URI uri, String path, IProgressMonitor monitor) throws CoreException
+	{
+		List<String> versionList = null;
+
+		Document doc = getMetadataDocument(readerType, uri, path + "maven-metadata.xml", monitor);
+		if (doc != null)
+		{
+			Element versioningElement = (Element) doc.getElementsByTagName("versioning").item(0);
+			if(versioningElement != null)
+			{
+				Element versionsElement = (Element) versioningElement.getElementsByTagName("versions").item(0);
+				if(versionsElement != null)
+				{
+					NodeList versions = versionsElement.getElementsByTagName("version");
+					int top = versions.getLength();
+					for (int i = 0; i < top; i++)
+					{
+						String version = versions.item(i).getTextContent();
+						if (isSnapshotVersion(versionDesignator) == isSnapshotVersion(version))
+						{
+							if(versionList == null)
+								versionList = new ArrayList<String>();
+							versionList.add(version);
+						}
+					}
+				}
+			}
+		}
+		return versionList == null ? Collections.<String>emptyList() : versionList;
+	}
+
+	private void appendSnapshotIdentifier(StringBuilder pbld, Maven2ReaderType readerType, URI uri, String rootPath, String version, IProgressMonitor monitor) throws CoreException
+	{
+		Document doc = getMetadataDocument(readerType, uri, rootPath + version + "/" + "maven-metadata.xml", monitor);
+		if(doc != null)
+		{
+			Element versioningElement = (Element) doc.getElementsByTagName("versioning").item(0);
+			if(versioningElement != null)
+			{
+				Element versionsElement = (Element) versioningElement.getElementsByTagName("snapshot").item(0);
+				if(versionsElement != null)
+				{
+					Element timestampElement = (Element) versionsElement.getElementsByTagName("timestamp").item(0);
+					Element buildNumElement = (Element) versionsElement.getElementsByTagName("buildNumber").item(0);
+					if(timestampElement != null && buildNumElement != null)
+					{
+						pbld.append(version.substring(0, version.indexOf("SNAPSHOT")));
+						pbld.append(timestampElement.getTextContent());
+						pbld.append('-');
+						pbld.append(buildNumElement.getTextContent());
+						return;
+					}
+				}
+			}
+		}
+		throw BuckminsterException.fromMessage("Unable to read snapshot metadata");
+	}
+
+	private boolean isSnapshotVersion(String version)
+	{
+		return version.endsWith("SNAPSHOT");
+	}
+
+	private boolean isSnapshotVersion(IVersionDesignator versionDesignator)
+	{
+		if (versionDesignator != null)
+			return isSnapshotVersion(versionDesignator.getVersion().toString());
+		return false;
+	}
+
+	private Document getMetadataDocument(Maven2ReaderType readerType, URI uri, String path, IProgressMonitor monitor) throws CoreException
+	{
+		URL url = readerType.createURL(uri, path);
+
+		Document doc;
+		try
+		{
+			doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(CorePlugin.getDefault().openCachedURL(url, monitor));
+		}
+		catch(CoreException e)
+		{
+			throw e;
+		}
+		catch(Exception e)
+		{
+			throw BuckminsterException.wrap(e);
+		}
+
+		// Make sure groupId and artifactId in metadata document match map entry
+		String mapEntryGroupId = getMapEntry().getGroupId();
+		String mapEntryArtifactId = getMapEntry().getArtifactId();
+
+		String docGroupId = doc.getElementsByTagName("groupId").item(0).getTextContent();
+		String docArtifactId = doc.getElementsByTagName("artifactId").item(0).getTextContent();
+
+		if (mapEntryGroupId.equals(docGroupId) && mapEntryArtifactId.equals(docArtifactId))
+			return doc;
+
+		CorePlugin.getLogger().warning("Metadata at %s has groupId:artifactId %s:%s  that does not match map entry %s:%s",
+			url, docGroupId, docArtifactId, mapEntryGroupId, mapEntryArtifactId);
+		return null;
 	}
 }
