e4ff0015a35bb4e1bcf2fbb5d6ba2637f2d5babc
Thomas Hallgren <thomas@tada.se>
2008-01-28 23:40:52 +0000

    Made Buckminster visible in the Plug-in Dependencies view.

--- org.eclipse.buckminster.pde.ui/src/org/eclipse/buckminster/pde/ui/actions/InvokeBundleAction.java
+++ org.eclipse.buckminster.pde.ui/src/org/eclipse/buckminster/pde/ui/actions/InvokeBundleAction.java
@@ -1,10 +1,10 @@
 package org.eclipse.buckminster.pde.ui.actions;
 
-import org.eclipse.buckminster.ui.actions.ViewCSpecAction;
+import org.eclipse.buckminster.ui.actions.InvokeAction;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 
-public class ViewBundleCSpecAction extends ViewCSpecAction
+public class InvokeBundleAction extends InvokeAction
 {
 	@Override
 	public void selectionChanged(IAction action, ISelection selection)
