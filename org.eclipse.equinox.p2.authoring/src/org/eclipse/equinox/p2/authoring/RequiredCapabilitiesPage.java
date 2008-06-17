468b6ae08d5be89900dc0f78f33bdb9afac535a8
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2008-06-17 00:29:55 +0000

    cleanup of dead code.

--- org.eclipse.equinox.p2.authoring/src/org/eclipse/equinox/p2/authoring/RequiredCapabilitiesPage.java
+++ org.eclipse.equinox.p2.authoring/src/org/eclipse/equinox/p2/authoring/RequiredCapabilitiesPage.java
@@ -18,9 +18,15 @@ import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.widgets.TableWrapData;
 
+/**
+ * Page for editing Installable Unit Required Capabilities.
+ * @author Henrik Lindberg
+ *
+ */
 public class RequiredCapabilitiesPage extends RichFormPage implements IPageMementoProvider
 {
 	public static final String PAGE_ID = "required.id"; //$NON-NLS-1$
+
 	private final RequiredBodyBlock m_requiredBodyBlock;
 
 	public RequiredCapabilitiesPage(FormEditor editor)
@@ -28,9 +34,9 @@ public class RequiredCapabilitiesPage extends RichFormPage implements IPageMemen
 		super(editor, PAGE_ID, "Required Capabilities");
 		m_header = "Required Capabilities";
 		m_headerImage = P2AuthoringImages.getImage(P2AuthoringImages.IMG_REQ_CAPABILITIES);
-		
+
 		m_numColumns = 1;
-		TableWrapData wrapData = new TableWrapData(TableWrapData.FILL_GRAB,TableWrapData.FILL_GRAB);
+		TableWrapData wrapData = new TableWrapData(TableWrapData.FILL_GRAB, TableWrapData.FILL_GRAB);
 		wrapData.indent = 0;
 
 		m_requiredBodyBlock = new RequiredBodyBlock(this, wrapData);
@@ -39,47 +45,8 @@ public class RequiredCapabilitiesPage extends RichFormPage implements IPageMemen
 	@Override
 	protected void addFormContent(IManagedForm managedForm)
 	{
-//		final ScrolledForm scrolledForm = managedForm.getForm();
-//		FormToolkit toolkit = managedForm.getToolkit();
-		
-//		Section section = createGeneralSection(toolkit, scrolledForm.getBody());
-//		section.addExpansionListener(getReflowListener());
-		
 		m_requiredBodyBlock.createContent(managedForm);
-				
 	}
-//	private Section createGeneralSection(FormToolkit toolkit, Composite parent)
-//	{
-//		Section section = toolkit.createSection(parent, //
-//				Section.DESCRIPTION | //
-//				Section.TITLE_BAR | //
-//				Section.TWISTIE | // Expandable by user
-//				Section.EXPANDED);
-//
-//		TableWrapData td = new TableWrapData(TableWrapData.FILL_GRAB, TableWrapData.TOP);
-//		td.colspan = 1;
-//		section.setLayoutData(td);
-//		section.setText("General");
-//		section.setDescription("General information about the collection of feeds.");
-//		Composite sectionClient = toolkit.createComposite(section);
-//		GridLayout layout = new GridLayout(2, false);
-//		sectionClient.setLayout(layout);
-//		
-//		FormColors colors = toolkit.getColors();
-//		Label label = toolkit.createLabel(sectionClient, "Author:");
-//		label.setForeground(colors.getColor("org.eclipse.ui.forms.TITLE"));
-//		Text text = toolkit.createText(sectionClient, "");
-//		text.setLayoutData(new GridData(SWT.FILL,SWT.CENTER, true,false));
-////		text.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_YELLOW));
-//
-//		label = toolkit.createLabel(sectionClient, "Email:");
-//		label.setForeground(colors.getColor("org.eclipse.ui.forms.TITLE"));
-//		text = toolkit.createText(sectionClient, "");
-//		text.setLayoutData(new GridData(SWT.FILL,SWT.CENTER, true,false));
-//		
-//		section.setClient(sectionClient);
-//		return section;
-//	}
 
 	public Object getPageMemento()
 	{
@@ -90,6 +57,4 @@ public class RequiredCapabilitiesPage extends RichFormPage implements IPageMemen
 	{
 		m_requiredBodyBlock.setPageMemento(memento);
 	}
-	
-
 }
