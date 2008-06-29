c51a4f9a73c74b419b5bd1a8d967b7cee67cd77d
Karel Brezina <karel.brezina@cloudsmith.com>
2008-06-29 20:02:41 +0000

    Done page was restyled

--- org.eclipse.buckminster.jnlp/src/org/eclipse/buckminster/jnlp/wizard/install/DonePage.java
+++ org.eclipse.buckminster.jnlp/src/org/eclipse/buckminster/jnlp/wizard/install/DonePage.java
@@ -8,8 +8,6 @@
 
 package org.eclipse.buckminster.jnlp.wizard.install;
 
-import java.util.List;
-
 import org.eclipse.buckminster.core.materializer.MaterializationContext;
 import org.eclipse.buckminster.core.materializer.MaterializationStatistics;
 import org.eclipse.buckminster.jnlp.MaterializationConstants;
@@ -36,16 +34,27 @@ public class DonePage extends InstallWizardPage
 {
 	private static final int VERTICAL_SPACING = 10;
 
-	private static final int HORIZONTAL_INDENT = 50;
+	private static final int HORIZONTAL_INDENT = 20;
 
-	private static final String ICON_LEARN = "library_wiz.png";
+	private static final String ICON_ARROW = "incom_stat.gif";
+
+	private static final String ICON_EXCLAMATION = "hprio_tsk.gif";
 
 	private ComponentListPanel m_componentListPanel;
 
+	private Label m_heading1;
+
+	private Label m_heading2;
+
+	private Label m_heading3;
+
+	private Label m_heading4;
+
+	private Label m_heading5;
+
 	protected DonePage()
 	{
-		super(MaterializationConstants.STEP_DONE, "Materialization Completed", "Close the materialization dialog.",
-				null);
+		super(MaterializationConstants.STEP_DONE, "View Results", "Materialization of distro completed.", null);
 		setPreviousPage(this);
 	}
 
@@ -55,37 +64,99 @@ public class DonePage extends InstallWizardPage
 		GridLayout layout = new GridLayout(2, false);
 		layout.verticalSpacing = VERTICAL_SPACING;
 		pageComposite.setLayout(layout);
+		pageComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-		Label label = new Label(pageComposite, SWT.NONE);
-		label.setText("Learn More:");
+		m_heading1 = new Label(pageComposite, SWT.WRAP);
+		m_heading1.setText("The components that were downloaded, and their destination folders, are listed below.");
 		GridData layoutData = new GridData();
 		layoutData.horizontalSpan = 2;
+		m_heading1.setLayoutData(layoutData);
+
+		Label space = new Label(pageComposite, SWT.NONE);
+		layoutData = new GridData();
+		layoutData.horizontalSpan = 2;
+		layoutData.heightHint = 5;
+		space.setLayoutData(layoutData);
+
+		Label label = new Label(pageComposite, SWT.NONE);
+		label.setImage(MaterializationUtils.getImage(ICON_ARROW));
+		layoutData = new GridData();
+		layoutData.horizontalIndent = HORIZONTAL_INDENT;
+		layoutData.verticalAlignment = GridData.BEGINNING;
 		label.setLayoutData(layoutData);
 
-		List<LearnMoreItem> learnMores = getInstallWizard().getLearnMores();
+		m_heading2 = new Label(pageComposite, SWT.WRAP);
+		layoutData = new GridData();
+		layoutData.horizontalIndent = HORIZONTAL_INDENT;
+		m_heading2.setLayoutData(layoutData);
 
 		label = new Label(pageComposite, SWT.NONE);
-		label.setImage(MaterializationUtils.getImage(ICON_LEARN));
-		GridData learnMoreIconlayoutData = new GridData();
-		learnMoreIconlayoutData.horizontalIndent = HORIZONTAL_INDENT;
+		label.setImage(MaterializationUtils.getImage(ICON_ARROW));
+		layoutData = new GridData();
+		layoutData.horizontalIndent = HORIZONTAL_INDENT;
+		layoutData.verticalAlignment = GridData.BEGINNING;
+		label.setLayoutData(layoutData);
 
-		learnMoreIconlayoutData.verticalSpan = learnMores.size();
-		if(learnMoreIconlayoutData.verticalSpan == 0)
-		{
-			learnMoreIconlayoutData.verticalSpan = 1;
-			new Label(pageComposite, SWT.NONE);
-		}
+		m_heading3 = new Label(pageComposite, SWT.WRAP);
+		layoutData = new GridData();
+		layoutData.horizontalIndent = HORIZONTAL_INDENT;
+		m_heading3.setLayoutData(layoutData);
 
-		learnMoreIconlayoutData.verticalAlignment = GridData.CENTER;
-		label.setLayoutData(learnMoreIconlayoutData);
+		label = new Label(pageComposite, SWT.NONE);
+		label.setImage(MaterializationUtils.getImage(ICON_ARROW));
+		layoutData = new GridData();
+		layoutData.horizontalIndent = HORIZONTAL_INDENT;
+		layoutData.verticalAlignment = GridData.BEGINNING;
+		label.setLayoutData(layoutData);
 
-		for(LearnMoreItem item : learnMores)
-		{
-			createLink(pageComposite, item.getString(), item.getUrl());
-		}
+		m_heading4 = new Label(pageComposite, SWT.WRAP);
+		layoutData = new GridData();
+		layoutData.horizontalIndent = HORIZONTAL_INDENT;
+		m_heading4.setLayoutData(layoutData);
 
 		new Label(pageComposite, SWT.NONE);
-		new Label(pageComposite, SWT.NONE);
+
+		Composite exclamationComposite = new Composite(pageComposite, SWT.NONE);
+		layout = new GridLayout(2, false);
+		layout.marginHeight = layout.marginWidth = 0;
+		exclamationComposite.setLayout(layout);
+		
+		label = new Label(exclamationComposite, SWT.NONE);
+		label.setImage(MaterializationUtils.getImage(ICON_EXCLAMATION));
+		layoutData = new GridData();
+		layoutData.horizontalIndent = HORIZONTAL_INDENT;
+		label.setLayoutData(layoutData);
+
+		m_heading5 = new Label(exclamationComposite, SWT.WRAP);
+
+		space = new Label(pageComposite, SWT.NONE);
+		layoutData = new GridData();
+		layoutData.horizontalSpan = 2;
+		layoutData.heightHint = 5;
+		space.setLayoutData(layoutData);
+		
+		Link learnMore = new Link(pageComposite, SWT.NONE);
+		learnMore.setText("Click <a>here</a> to learn more about " + getInstallWizard().getServiceProvider() + ".");
+		layoutData = new GridData();
+		layoutData.horizontalSpan = 2;
+		learnMore.setLayoutData(layoutData);
+		learnMore.addSelectionListener(new SelectionAdapter()
+		{
+			@Override
+			public void widgetSelected(SelectionEvent e)
+			{
+				if(getInstallWizard().getServiceProviderHomePageURL() != null)
+				{
+					Program.launch(getInstallWizard().getServiceProviderHomePageURL());
+				}
+			}
+		});
+
+		space = new Label(pageComposite, SWT.NONE);
+		layoutData = new GridData();
+		layoutData.horizontalSpan = 2;
+		layoutData.heightHint = 5;
+		space.setLayoutData(layoutData);
 
 		Group listGroup = new Group(pageComposite, SWT.NONE);
 		listGroup.setText("Materialized Components");
@@ -98,33 +169,8 @@ public class DonePage extends InstallWizardPage
 
 		m_componentListPanel = new ComponentListPanel();
 		m_componentListPanel.createControl(listGroup);
-		
+
 		setControl(pageComposite);
-	}
-
-	private Link createLink(Composite parent, String string, final String url)
-	{
-		Link learnMoreLink = new Link(parent, SWT.NONE);
-		learnMoreLink.setText("<a>" + string + "</a>");
-		GridData layoutData = new GridData();
-		layoutData.horizontalIndent = HORIZONTAL_INDENT;
-		learnMoreLink.setLayoutData(layoutData);
-		learnMoreLink.addSelectionListener(new SelectionAdapter()
-		{
-
-			@Override
-			public void widgetSelected(SelectionEvent e)
-			{
-				String linkURL = url;
-
-				if(linkURL != null)
-				{
-					Program.launch(linkURL);
-				}
-			}
-		});
-
-		return learnMoreLink;
 	}
 
 	public void update(MaterializationContext context)
@@ -143,6 +189,34 @@ public class DonePage extends InstallWizardPage
 				setErrorMessage("Materialization of 1 component was cancelled");
 		else
 			setErrorMessage(null);
+	}
+
+	@Override
+	protected void beforeDisplaySetup()
+	{
+
+		// Text of the label is set here to be able to WRAP it - no idea how to do it nicer
+		m_heading2
+				.setText("If you materialized  to an Eclipse workspace, you should be able to select the new workspace directly from within Eclipse.");
+		GridData layoutData = (GridData)m_heading2.getLayoutData();
+		layoutData.widthHint = m_heading2.getShell().getSize().x - 35 - 2 * HORIZONTAL_INDENT - 11;
+
+		m_heading3
+				.setText("If you materialized to an Eclipse target platform, the new functionality should be available next time you start Eclipse.");
+		layoutData = (GridData)m_heading3.getLayoutData();
+		layoutData.widthHint = m_heading3.getShell().getSize().x - 35 - 2 * HORIZONTAL_INDENT - 11;
+
+		m_heading4
+				.setText("If you materialized to a file system, the materialized content should be present in the designated location.");
+		layoutData = (GridData)m_heading4.getLayoutData();
+		layoutData.widthHint = m_heading4.getShell().getSize().x - 35 - 2 * HORIZONTAL_INDENT - 11;
+
+		m_heading5
+				.setText("Make sure to note this location before exiting the wizard.");
+		layoutData = (GridData)m_heading5.getLayoutData();
+		layoutData.widthHint = m_heading5.getShell().getSize().x - 50 - 2 * HORIZONTAL_INDENT - 11;
+
+		m_heading1.getParent().layout();
 	}
 
 	// Previous is disabled
