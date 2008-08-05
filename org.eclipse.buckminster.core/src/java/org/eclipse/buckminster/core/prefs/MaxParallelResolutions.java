828f1f5a918c09664f4d454976cf29f7ce7b3b68
Thomas Hallgren <thomas@tada.se>
2008-08-05 11:17:55 +0000

    ASSIGNED - bug 242782: Expose maximum resolution threads preference in headless mode https://bugs.eclipse.org/bugs/show_bug.cgi?id=242782

---
+++ org.eclipse.buckminster.core/src/java/org/eclipse/buckminster/core/prefs/MaxParallelResolutions.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2007, Cloudsmith Inc.
+ * The code, documentation and other materials contained herein have been
+ * licensed under the Eclipse Public License - v 1.0 by the copyright holder
+ * listed above, as the Initial Contributor under such license. The text of
+ * such license is available at www.eclipse.org.
+ ******************************************************************************/
+
+package org.eclipse.buckminster.core.prefs;
+
+import org.eclipse.buckminster.cmdline.BasicPreferenceHandler;
+import org.eclipse.buckminster.core.resolver.ResourceMapResolverFactory;
+import org.eclipse.core.runtime.CoreException;
+import org.osgi.service.prefs.BackingStoreException;
+
+/**
+ * @author thhal
+ *
+ */
+public class MaxParallelResolutions extends BasicPreferenceHandler
+{
+	@Override
+	public String get(String defaultValue) throws CoreException
+	{
+		return super.get(Integer.toString(ResourceMapResolverFactory.RESOLVER_THREADS_MAX_DEFAULT));
+	}
+
+	@Override
+	public void set(String prefValue) throws BackingStoreException
+	{
+		try
+		{
+			int maxJobs = Integer.parseInt(prefValue);
+			if(maxJobs > 0 && maxJobs <= 20)
+			{
+				super.set(prefValue);
+				return;
+			}
+		}
+		catch(NumberFormatException e)
+		{
+		}
+		throw new IllegalArgumentException(
+			String.format("%s is an illegal value for maxParallelResolutions. It must be an integer value between 1 and 20", prefValue));
+			
+	}
+
+	@Override
+	public void unset() throws BackingStoreException
+	{
+		set(Integer.toString(ResourceMapResolverFactory.RESOLVER_THREADS_MAX_DEFAULT));
+	}
+}
