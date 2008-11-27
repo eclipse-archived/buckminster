f8b29b75c05c080f44a7be5dfe2c795ea5ce0486
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2008-11-27 00:15:05 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=253521

--- org.eclipse.buckminster.remote/src/java/org/eclipse/buckminster/remote/RemoteConstants.java
+++ org.eclipse.buckminster.remote/src/java/org/eclipse/buckminster/remote/RemoteConstants.java
@@ -17,27 +17,27 @@ import org.eclipse.buckminster.runtime.Buckminster;
  */
 public final class RemoteConstants
 {
-	public static final String REMOVE_NAMESPACE = Buckminster.NAMESPACE + ".remote";
+	public static final String REMOVE_NAMESPACE = Buckminster.NAMESPACE + ".remote"; //$NON-NLS-1$
 
-	public static final String PROVIDERS_POINT = REMOVE_NAMESPACE + ".providers";
+	public static final String PROVIDERS_POINT = REMOVE_NAMESPACE + ".providers"; //$NON-NLS-1$
 
-	public static final String PROVIDER_ID_ATTR = "ID";
+	public static final String PROVIDER_ID_ATTR = "ID"; //$NON-NLS-1$
 
-	public static final String PROVIDER_NAME_ATTR = "name";
+	public static final String PROVIDER_NAME_ATTR = "name"; //$NON-NLS-1$
 
-	public static final String PROVIDER_PROTOCOL_ATTR = "protocol";
+	public static final String PROVIDER_PROTOCOL_ATTR = "protocol"; //$NON-NLS-1$
 
-	public static final String PROVIDER_HOSTNAME_ATTR = "hostname";
+	public static final String PROVIDER_HOSTNAME_ATTR = "hostname"; //$NON-NLS-1$
 
-	public static final String PROVIDER_PORT_ATTR = "port";
+	public static final String PROVIDER_PORT_ATTR = "port"; //$NON-NLS-1$
 
-	public static final String PROVIDER_INITIALIZATION_PATH_ATTR = "initializationPath";
+	public static final String PROVIDER_INITIALIZATION_PATH_ATTR = "initializationPath"; //$NON-NLS-1$
 
-	public static final String PROVIDER_SERVICE_PATH_ATTR = "servicePath";
+	public static final String PROVIDER_SERVICE_PATH_ATTR = "servicePath"; //$NON-NLS-1$
 
-	public static final String PROVIDER_LOGIN_SUPPORT = "loginSupport";
+	public static final String PROVIDER_LOGIN_SUPPORT = "loginSupport"; //$NON-NLS-1$
 
-	public static final String BOOLEAN_TRUE = "true";
+	public static final String BOOLEAN_TRUE = "true"; //$NON-NLS-1$
 
-	public static final String BOOLEAN_FALSE = "false";
+	public static final String BOOLEAN_FALSE = "false"; //$NON-NLS-1$
 }
