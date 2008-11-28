04d655234225433d09c322887365d10c7c116615
Michal Ruzicka <michal.ruzicka@cloudsmith.com>
2008-11-28 02:52:17 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=256518

--- org.eclipse.buckminster.executor.test/src/org/eclipse/buckminster/executor/actor/EnvironnementVariablesTest.java
+++ org.eclipse.buckminster.executor.test/src/org/eclipse/buckminster/executor/actor/EnvironnementVariablesTest.java
@@ -6,20 +6,20 @@ import org.junit.Test;
 public class EnvironnementVariablesTest
 {
 	/**
-	 * Verifies the environment variables set in the CSPEC is split correctly
-	 * regarding the quoted parts.
+	 * Verifies the environment variables set in the CSPEC is split correctly regarding the quoted parts.
 	 */
 	@Test
 	public void testEnvironnementVariables()
 	{
-		final String[] variables = new String[] { "DevEnvDir=%VSINSTALLDIR%\\Common7\\IDE",
+		final String[] variables = new String[] {
+				"DevEnvDir=%VSINSTALLDIR%\\Common7\\IDE",
 				"PATH=\"%DevEnvDir%;%VCINSTALLDIR%\\BIN;%VSINSTALLDIR%\\Common7\\Tools;%VSINSTALLDIR%\\Common7\\Tools\\bin;%VCINSTALLDIR%\\PlatformSDK\\bin;%FrameworkSDKDir%\\bin;%FrameworkDir%\\%FrameworkVersion%;%VCINSTALLDIR%\\VCPackages;%PATH%\"",
 				"INCLUDE=\"%VCINSTALLDIR%\\ATLMFC\\INCLUDE;%VCINSTALLDIR%\\INCLUDE;%VCINSTALLDIR%\\PlatformSDK\\include;%FrameworkSDKDir%\\include;%INCLUDE%\"",
 				"LIB=\"%VCINSTALLDIR%\\ATLMFC\\LIB;%VCINSTALLDIR%\\LIB;%VCINSTALLDIR%\\PlatformSDK\\lib;%FrameworkSDKDir%\\lib;%LIB%\"",
 				"LIBPATH=\"%FrameworkDir%\\%FrameworkVersion%;%VCINSTALLDIR%\\ATLMFC\\LIB\"" };
 		String concat = "";
 		for(String string : variables)
-			concat += concat.length()==0
+			concat += concat.length() == 0
 					? string
 					: ';' + string;
 
