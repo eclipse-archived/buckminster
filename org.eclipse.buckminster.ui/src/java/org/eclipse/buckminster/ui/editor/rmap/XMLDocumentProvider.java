c7f2e026992eb59328d51302c540bc93e0d6cb88
Thomas Hallgren <thomas@tada.se>
2008-12-10 10:07:19 +0000

    Fixed https://bugs.eclipse.org/bugs/show_bug.cgi?id=250379

--- org.eclipse.buckminster.ui/src/java/org/eclipse/buckminster/ui/editor/rmap/XMLDocumentProvider.java
+++ org.eclipse.buckminster.ui/src/java/org/eclipse/buckminster/ui/editor/rmap/XMLDocumentProvider.java
@@ -1,17 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2008, Cloudsmith Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Cloudsmith Inc. - initial API and implementation
+ *     Remy Chi Jian Suen (Versant Corp.) - Bug 250379
+ *******************************************************************************/
 package org.eclipse.buckminster.ui.editor.rmap;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentPartitioner;
 import org.eclipse.jface.text.rules.FastPartitioner;
-import org.eclipse.ui.editors.text.FileDocumentProvider;
+import org.eclipse.ui.editors.text.TextFileDocumentProvider;
 
-public class XMLDocumentProvider extends FileDocumentProvider
+public class XMLDocumentProvider extends TextFileDocumentProvider
 {
+
 	@Override
-	protected IDocument createDocument(Object element) throws CoreException
+	protected FileInfo createFileInfo(Object element) throws CoreException
 	{
-		IDocument document = super.createDocument(element);
+		FileInfo fileInfo = super.createFileInfo(element);
+		if(fileInfo == null)
+		{
+			return null;
+		}
+
+		IDocument document = fileInfo.fTextFileBuffer.getDocument();
 		if(document != null)
 		{
 			IDocumentPartitioner partitioner = new FastPartitioner(new XMLPartitionScanner(), new String[] {
@@ -19,6 +37,7 @@ public class XMLDocumentProvider extends FileDocumentProvider
 			partitioner.connect(document);
 			document.setDocumentPartitioner(partitioner);
 		}
-		return document;
+
+		return fileInfo;
 	}
 }
