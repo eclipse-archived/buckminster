5d6ec5320f775e87c74a7b51507050014680509d
Thomas Hallgren <thomas@tada.se>
2009-02-10 23:16:35 +0000

    Changed the P2Materializer. No planning. This to avoid problems with platform specifics

--- org.eclipse.buckminster.core/src/java/org/eclipse/buckminster/core/materializer/P2Materializer.java
+++ org.eclipse.buckminster.core/src/java/org/eclipse/buckminster/core/materializer/P2Materializer.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -32,39 +33,27 @@ import org.eclipse.equinox.internal.provisional.p2.artifact.repository.IArtifact
 import org.eclipse.equinox.internal.provisional.p2.core.ProvisionException;
 import org.eclipse.equinox.internal.provisional.p2.core.Version;
 import org.eclipse.equinox.internal.provisional.p2.core.VersionRange;
-import org.eclipse.equinox.internal.provisional.p2.director.IDirector;
-import org.eclipse.equinox.internal.provisional.p2.director.ProfileChangeRequest;
+import org.eclipse.equinox.internal.provisional.p2.engine.IEngine;
 import org.eclipse.equinox.internal.provisional.p2.engine.IProfile;
 import org.eclipse.equinox.internal.provisional.p2.engine.IProfileRegistry;
+import org.eclipse.equinox.internal.provisional.p2.engine.InstallableUnitOperand;
+import org.eclipse.equinox.internal.provisional.p2.engine.Phase;
+import org.eclipse.equinox.internal.provisional.p2.engine.PhaseSet;
+import org.eclipse.equinox.internal.provisional.p2.engine.ProvisioningContext;
+import org.eclipse.equinox.internal.provisional.p2.engine.phases.Collect;
 import org.eclipse.equinox.internal.provisional.p2.metadata.IInstallableUnit;
 import org.eclipse.equinox.internal.provisional.p2.metadata.query.InstallableUnitQuery;
 import org.eclipse.equinox.internal.provisional.p2.metadata.repository.IMetadataRepository;
 import org.eclipse.equinox.internal.provisional.p2.metadata.repository.IMetadataRepositoryManager;
 import org.eclipse.equinox.internal.provisional.p2.query.Collector;
-import org.eclipse.osgi.service.environment.EnvironmentInfo;
 import org.eclipse.osgi.util.NLS;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleException;
 
 @SuppressWarnings("restriction")
 public class P2Materializer extends AbstractMaterializer
 {
-	private static final String ECLIPSE_P2_PROFILE_ID = "eclipse.p2.profile.id"; //$NON-NLS-1$
-
-	private static final String SDK_PROFILE = "SDKProfile"; //$NON-NLS-1$
-
-	private static final String P2_SETUP_BUNDLE_NAME = "org.eclipse.equinox.p2.exemplarysetup"; //$NON-NLS-1$
-
-	private static final String P2_CORE_BUNDLE_NAME = "org.eclipse.equinox.p2.core"; //$NON-NLS-1$
-
-	private static final String ECLIPSE_DATA_AREA_PROPERTY = "eclipse.p2.data.area"; //$NON-NLS-1$
-
-	public static IArtifactRepository getArtifactRepository(URI repoLocation, IProgressMonitor monitor)
-			throws CoreException
+	static IArtifactRepository getArtifactRepository(IArtifactRepositoryManager manager, URI repoLocation,
+			IProgressMonitor monitor) throws CoreException
 	{
-		CorePlugin bundle = CorePlugin.getDefault();
-		repoLocation = cleanURIFromImportType(repoLocation);
-		IArtifactRepositoryManager manager = bundle.getService(IArtifactRepositoryManager.class);
 		SubMonitor subMon = SubMonitor.convert(monitor, 200);
 		try
 		{
@@ -73,21 +62,12 @@ public class P2Materializer extends AbstractMaterializer
 		catch(ProvisionException e)
 		{
 			return manager.refreshRepository(repoLocation, subMon.newChild(100));
-		}
-		finally
-		{
-			bundle.ungetService(manager);
-			if(monitor != null)
-				monitor.done();
 		}
 	}
 
-	public static IMetadataRepository getMetadataRepository(URI repoLocation, IProgressMonitor monitor)
-			throws CoreException
+	static IMetadataRepository getMetadataRepository(IMetadataRepositoryManager manager, URI repoLocation,
+			IProgressMonitor monitor) throws CoreException
 	{
-		CorePlugin bundle = CorePlugin.getDefault();
-		repoLocation = cleanURIFromImportType(repoLocation);
-		IMetadataRepositoryManager manager = bundle.getService(IMetadataRepositoryManager.class);
 		SubMonitor subMon = SubMonitor.convert(monitor, 200);
 		try
 		{
@@ -96,12 +76,6 @@ public class P2Materializer extends AbstractMaterializer
 		catch(ProvisionException e)
 		{
 			return manager.refreshRepository(repoLocation, subMon.newChild(100));
-		}
-		finally
-		{
-			bundle.ungetService(manager);
-			if(monitor != null)
-				monitor.done();
 		}
 	}
 
@@ -165,14 +139,8 @@ public class P2Materializer extends AbstractMaterializer
 				}
 			}
 
-			String profileId = (String)props.get(ECLIPSE_P2_PROFILE_ID);
-			if(profileId == null)
-				profileId = SDK_PROFILE;
-
 			if(installLocation == null)
-				installLocation = installRoot.append(profileId);
-			else
-				installLocation = installLocation.append(profileId);
+				installLocation = installRoot;
 
 			List<Resolution> rss = resPerLocation.get(installLocation);
 			if(rss == null)
@@ -184,149 +152,131 @@ public class P2Materializer extends AbstractMaterializer
 		}
 
 		CorePlugin bundle = CorePlugin.getDefault();
-		Bundle p2SetUp = Platform.getBundle(P2_SETUP_BUNDLE_NAME);
-		if(p2SetUp == null)
-			throw BuckminsterException.fromMessage(NLS.bind(Messages.Missing_OSGi_Bundle_0, P2_SETUP_BUNDLE_NAME));
-
-		Bundle p2Core = Platform.getBundle(P2_CORE_BUNDLE_NAME);
-		if(p2Core == null)
-			throw BuckminsterException.fromMessage(NLS.bind(Messages.Missing_OSGi_Bundle_0, P2_SETUP_BUNDLE_NAME));
-
-		String origLocation = System.getProperty(ECLIPSE_DATA_AREA_PROPERTY);
-		boolean wasStarted = (p2SetUp.getState() == Bundle.ACTIVE);
-
 		SubMonitor subMon = SubMonitor.convert(monitor, resPerLocation.size() * 1000);
+		List<URI> mdrsToRemove = null;
+		List<URI> arsToRemove = null;
+		IMetadataRepositoryManager mdrManager = bundle.getService(IMetadataRepositoryManager.class);
+		IArtifactRepositoryManager arManager = bundle.getService(IArtifactRepositoryManager.class);
+		IEngine engine = bundle.getService(IEngine.class);
+		IProfileRegistry registry = bundle.getService(IProfileRegistry.class);
 		try
 		{
-			if(wasStarted)
-			{
-				p2SetUp.stop(Bundle.STOP_TRANSIENT);
-				p2Core.stop(Bundle.STOP_TRANSIENT);
-			}
-
 			for(Map.Entry<IPath, List<Resolution>> entry : resPerLocation.entrySet())
 			{
 				IPath installLocation = entry.getKey();
-				String profileId = installLocation.lastSegment();
+				List<Resolution> ress = entry.getValue();
+				List<IInstallableUnit> ius = new ArrayList<IInstallableUnit>(ress.size());
+				for(Resolution res : entry.getValue())
+				{
+					SubMonitor subSubMon = subMon.newChild(800 / ress.size());
+					subSubMon.setWorkRemaining(1000);
+					URI repoURI = cleanURIFromImportType(URI.create(res.getRepository()));
+					if(mdrManager.contains(repoURI))
+					{
+						if(mdrsToRemove == null)
+							mdrsToRemove = new ArrayList<URI>();
+						mdrsToRemove.add(repoURI);
+					}
 
-				installLocation = installLocation.removeLastSegments(1);
-				System.setProperty(ECLIPSE_DATA_AREA_PROPERTY, installLocation.toOSString());
-				p2Core.start(Bundle.START_TRANSIENT);
-				p2SetUp.start(Bundle.START_TRANSIENT);
+					IMetadataRepository mdr = getMetadataRepository(mdrManager, repoURI, subSubMon.newChild(500));
+					IComponentIdentifier cid = res.getComponentIdentifier();
+					Version version = new Version(cid.getVersion().toString());
+					VersionRange range = new VersionRange(version, true, version, true);
+					Collector collector = new Collector();
+					String name = cid.getName();
+					boolean isFeature = IComponentType.ECLIPSE_FEATURE.equals(cid.getComponentTypeID());
 
+					if(isFeature)
+						// Since this is what we want in the target platform
+						name = name + ".feature.jar"; //$NON-NLS-1$
+
+					mdr.query(new InstallableUnitQuery(name, range), collector, subSubMon.newChild(250));
+					Iterator<?> itor = collector.iterator();
+					if(!itor.hasNext())
+						throw new ProvisionException(NLS.bind(Messages.Unable_to_resolve_0_1_in_MDR_2, new Object[] {
+								cid.getName(), version, res.getRepository() }));
+
+					IInstallableUnit iu = (IInstallableUnit)itor.next();
+					ius.add(iu);
+
+					// Check if this IU has artifacts and if so, load the artifact repository
+					//
+					if(iu.getArtifacts().length > 0)
+					{
+						if(arManager.contains(repoURI))
+						{
+							if(arsToRemove == null)
+								arsToRemove = new ArrayList<URI>();
+							arsToRemove.add(repoURI);
+						}
+						getArtifactRepository(arManager, repoURI, subSubMon.newChild(250));
+					}
+					else
+						subSubMon.worked(250);
+				}
+
+				// create the operands from the list of IUs
+				InstallableUnitOperand[] operands = new InstallableUnitOperand[ius.size()];
+				int i = 0;
+				for(IInstallableUnit iu : ius)
+					operands[i++] = new InstallableUnitOperand(null, iu);
+
+				// ensure the user-specified artifact repos will be consulted by loading them
+
+				java.io.File destDir = installLocation.toFile();
+
+				// do a create here to ensure that we don't default to a #load later and grab a repo which is the wrong
+				// type
+				// e.g. extension location type because a plugins/ directory exists.
 				try
 				{
-					IProfile profile = null;
-					IProfileRegistry profileRegistry = bundle.getService(IProfileRegistry.class);
-					try
-					{
-						for(IProfile p : profileRegistry.getProfiles())
-							if(profileId.equals(p.getProfileId()))
-							{
-								profile = p;
-								break;
-							}
+					arManager.createRepository(destDir.toURI(), "Runnable repository.", //$NON-NLS-1$
+							IArtifactRepositoryManager.TYPE_SIMPLE_REPOSITORY, null);
+				}
+				catch(ProvisionException e)
+				{
+					// ignore... perhaps one already exists and we will just load it later
+				}
 
-						if(profile == null)
-						{
-							Map<String, String> properties = new HashMap<String, String>();
-							properties.put(IProfile.PROP_INSTALL_FOLDER, installLocation.toString());
-							EnvironmentInfo info = bundle.getService(EnvironmentInfo.class);
-							String env = "osgi.os=" + info.getOS() + ",osgi.ws=" + info.getWS() + ",osgi.arch=" + info.getOSArch(); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-							properties.put(IProfile.PROP_ENVIRONMENTS, env);
-							properties.put(IProfile.PROP_CACHE, installLocation.toString());
-							properties.put(IProfile.PROP_FLAVOR, "tooling"); //$NON-NLS-1$
-							properties.put(IProfile.PROP_ROAMING, "true"); //$NON-NLS-1$
-							properties.put(IProfile.PROP_INSTALL_FEATURES, "true"); //$NON-NLS-1$
-							properties.put(IProfile.PROP_NAME, profileId);
-							profile = profileRegistry.addProfile(profileId, properties);
-						}
-					}
-					finally
-					{
-						bundle.ungetService(profileRegistry);
-					}
+				// call the engine with only the "collect" phase so all we do is download
 
-					ProfileChangeRequest request = new ProfileChangeRequest(profile);
-					List<Resolution> ress = entry.getValue();
-					for(Resolution res : ress)
-					{
-						SubMonitor subSubMon = subMon.newChild(200 / ress.size());
-
-						subSubMon.setWorkRemaining(1000);
-						URI repoURI = URI.create(res.getRepository());
-						IMetadataRepository mdr = getMetadataRepository(repoURI, subSubMon.newChild(500));
-						IComponentIdentifier cid = res.getComponentIdentifier();
-						Version version = new Version(cid.getVersion().toString());
-						VersionRange range = new VersionRange(version, true, version, true);
-						Collector collector = new Collector();
-						String name = cid.getName();
-						if(IComponentType.ECLIPSE_FEATURE.equals(cid.getComponentTypeID())
-								&& !name.endsWith(".feature.group")) //$NON-NLS-1$
-							name = name + ".feature.group"; //$NON-NLS-1$
-
-						mdr.query(new InstallableUnitQuery(name, range), collector, subSubMon.newChild(250));
-						if(collector.isEmpty())
-							throw new ProvisionException(NLS.bind(Messages.Unable_to_resolve_0_1_in_MDR_2,
-									new Object[] { cid.getName(), version, res.getRepository() }));
-
-						IInstallableUnit[] ius = (IInstallableUnit[])collector.toArray(IInstallableUnit.class);
-						request.addInstallableUnits(ius);
-
-						// Check if this IU has artifacts and if so, load the artifact repository
-						//
-						boolean hasArtifacts = false;
-						int idx = ius.length;
-						while(--idx >= 0 && !hasArtifacts)
-							hasArtifacts = ius[idx].getArtifacts().length > 0;
-						if(hasArtifacts)
-							getArtifactRepository(repoURI, subSubMon.newChild(250));
-						else
-							subSubMon.worked(250);
-					}
-
-					IDirector director = bundle.getService(IDirector.class);
-					try
-					{
-						IStatus status = director.provision(request, null, subMon.newChild(800));
-						if(status.getSeverity() == IStatus.ERROR)
-							throw BuckminsterException.wrap(status);
-					}
-					finally
-					{
-						bundle.ungetService(director);
-					}
+				String destDirStr = destDir.toString();
+				Map<String, String> properties = new HashMap<String, String>();
+				properties.put(IProfile.PROP_CACHE, destDirStr);
+				properties.put(IProfile.PROP_INSTALL_FOLDER, destDirStr);
+				IProfile profile = registry.addProfile(System.currentTimeMillis() + "-" + Math.random(), properties); //$NON-NLS-1$
+				try
+				{
+					PhaseSet phaseSet = new PhaseSet(new Phase[] { new Collect(100) })
+					{ /* nothing to override */
+					};
+					engine = bundle.getService(IEngine.class);
+					IStatus status = engine.perform(profile, phaseSet, operands, new ProvisioningContext(), subMon
+							.newChild(200));
+					if(status.getSeverity() == IStatus.ERROR)
+						throw BuckminsterException.wrap(status);
 				}
 				finally
 				{
-					p2SetUp.stop(Bundle.STOP_TRANSIENT);
-					p2Core.stop(Bundle.STOP_TRANSIENT);
+					registry.removeProfile(profile.getProfileId());
 				}
 			}
-			return Collections.emptyList();
-		}
-		catch(BundleException e)
-		{
-			throw BuckminsterException.wrap(e);
 		}
 		finally
 		{
-			monitor.done();
-			if(origLocation == null)
-				System.clearProperty(ECLIPSE_DATA_AREA_PROPERTY);
-			else
-				System.setProperty(ECLIPSE_DATA_AREA_PROPERTY, origLocation);
+			if(mdrsToRemove != null)
+				for(URI repoLocation : mdrsToRemove)
+					mdrManager.removeRepository(repoLocation);
 
-			if(wasStarted)
-				try
-				{
-					p2Core.start(Bundle.START_TRANSIENT);
-					p2SetUp.start(Bundle.START_TRANSIENT);
-				}
-				catch(BundleException e)
-				{
-					throw BuckminsterException.wrap(e);
-				}
+			if(arsToRemove != null)
+				for(URI repoLocation : arsToRemove)
+					arManager.removeRepository(repoLocation);
+
+			bundle.ungetService(mdrManager);
+			bundle.ungetService(arManager);
+			bundle.ungetService(registry);
+			bundle.ungetService(engine);
 		}
+		return Collections.emptyList();
 	}
 }
