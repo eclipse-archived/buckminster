e56414ea574b91c68a2d4b9cc6c22ad465025f05
Thomas Hallgren <thomas@tada.se>
2009-04-09 16:44:05 +0000

    Fixed issue with order of initialization.

--- org.eclipse.buckminster.core/src/java/org/eclipse/buckminster/core/helpers/DateAndTimeUtils.java
+++ org.eclipse.buckminster.core/src/java/org/eclipse/buckminster/core/helpers/DateAndTimeUtils.java
@@ -40,19 +40,19 @@ public abstract class DateAndTimeUtils
 
 	static
 	{
+		ISO_8601Format = new SimpleDateFormat(ISO_8601Pattern);
+		UTC = TimeZone.getTimeZone("UTC"); //$NON-NLS-1$
+		ISO_8601Format.setCalendar(Calendar.getInstance(UTC));
+
 		int idx = commonFormats.length;
 		commonFormatters = new DateFormat[idx];
 		while(--idx >= 0)
 		{
 			DateFormat dm = new SimpleDateFormat(commonFormats[idx]);
-			dm.setTimeZone(DateAndTimeUtils.UTC);
+			dm.setTimeZone(UTC);
 			dm.setLenient(false);
 			commonFormatters[idx] = dm;
 		}
-
-		ISO_8601Format = new SimpleDateFormat(ISO_8601Pattern);
-		UTC = TimeZone.getTimeZone("UTC"); //$NON-NLS-1$
-		ISO_8601Format.setCalendar(Calendar.getInstance(UTC));
 	}
 
 	/**
@@ -91,20 +91,6 @@ public abstract class DateAndTimeUtils
 		throw new ParseException(NLS.bind(Messages.Unable_to_parse_0_as_timestamp, timestampStr), 0);
 	}
 
-	private static Date parseSaneDate(DateFormat mf, String str) throws ParseException
-	{
-		long now = System.currentTimeMillis();
-		Date dt;
-		synchronized(mf)
-		{
-			dt = mf.parse(str);
-		}
-		long tm = dt.getTime();
-		if(tm > now + SANITY_THRESHOLD || tm < now - SANITY_THRESHOLD)
-			throw new ParseException(Messages.Bogus, 0);
-		return dt;
-	}
-
 	/**
 	 * Create a string from <code>date</code> that is formatted according to ISO-8601 format
 	 * &quot;yyyy-MM-dd'T'HH:mm:ss.SSSZ&quot; and using the UTC time zone.
@@ -122,5 +108,19 @@ public abstract class DateAndTimeUtils
 		{
 			return ISO_8601Format.format(date);
 		}
+	}
+
+	private static Date parseSaneDate(DateFormat mf, String str) throws ParseException
+	{
+		long now = System.currentTimeMillis();
+		Date dt;
+		synchronized(mf)
+		{
+			dt = mf.parse(str);
+		}
+		long tm = dt.getTime();
+		if(tm > now + SANITY_THRESHOLD || tm < now - SANITY_THRESHOLD)
+			throw new ParseException(Messages.Bogus, 0);
+		return dt;
 	}
 }
