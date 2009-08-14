b11b5a0f1504e079b8f999c3c3ca4b14f3840384
Thomas Hallgren <thomas@tada.se>
2009-08-14 08:43:35 +0000

    Bug 286413 - Launcher is not included in RCP product (take two, launcher picked from runtime when executable feature is missing)

--- org.eclipse.buckminster.pde/src/java/org/eclipse/buckminster/pde/tasks/ApplicationLauncherAction.java
+++ org.eclipse.buckminster.pde/src/java/org/eclipse/buckminster/pde/tasks/ApplicationLauncherAction.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 
 import org.eclipse.equinox.internal.p2.publisher.eclipse.ExecutablesDescriptor;
 import org.eclipse.equinox.internal.provisional.p2.core.Version;
+import org.eclipse.equinox.p2.publisher.AbstractPublisherAction;
 import org.eclipse.equinox.p2.publisher.IPublisherAction;
 
 @SuppressWarnings("restriction")
@@ -17,6 +18,8 @@ public class ApplicationLauncherAction extends org.eclipse.equinox.p2.publisher.
 
 	private final String m_flavor;
 
+	private final File m_location;
+
 	public ApplicationLauncherAction(String id, Version version, String flavor, String executableName, File location,
 			String[] configSpecs)
 	{
@@ -24,6 +27,19 @@ public class ApplicationLauncherAction extends org.eclipse.equinox.p2.publisher.
 		m_id = id;
 		m_version = version;
 		m_flavor = flavor;
+		m_location = location;
+	}
+
+	@Override
+	protected ExecutablesDescriptor computeExecutables(String configSpec)
+	{
+		// See if we know about an executables feature then use it as the source
+		ExecutablesDescriptor result = ExecutablesDescriptor.createExecutablesFromFeature(m_location, configSpec);
+		if(result != null)
+			return result;
+		// otherwise, assume that we are running against an Eclipse install and do the default thing
+		String os = AbstractPublisherAction.parseConfigSpec(configSpec)[1];
+		return ExecutablesDescriptor.createDescriptor(os, "eclipse", m_location); //$NON-NLS-1$
 	}
 
 	@Override
@@ -39,4 +55,5 @@ public class ApplicationLauncherAction extends org.eclipse.equinox.p2.publisher.
 		}
 		return actions;
 	}
+
 }
--- org.eclipse.buckminster.pde/src/java/org/eclipse/buckminster/pde/tasks/ProductAction.java
+++ org.eclipse.buckminster.pde/src/java/org/eclipse/buckminster/pde/tasks/ProductAction.java
@@ -9,8 +9,13 @@ package org.eclipse.buckminster.pde.tasks;
 
 import java.io.File;
 
+import org.eclipse.buckminster.core.TargetPlatform;
+import org.eclipse.buckminster.runtime.Buckminster;
+import org.eclipse.buckminster.runtime.Logger;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.equinox.internal.p2.publisher.eclipse.IProductDescriptor;
 import org.eclipse.equinox.internal.provisional.frameworkadmin.BundleInfo;
 import org.eclipse.equinox.internal.provisional.p2.metadata.IInstallableUnit;
@@ -20,6 +25,7 @@ import org.eclipse.equinox.p2.publisher.IPublisherResult;
 import org.eclipse.equinox.p2.publisher.PublisherInfo;
 import org.eclipse.equinox.p2.publisher.PublisherResult;
 import org.eclipse.equinox.p2.publisher.eclipse.EquinoxLauncherCUAction;
+import org.eclipse.osgi.util.NLS;
 
 /**
  * Action that generates version adjusted products
@@ -39,7 +45,48 @@ public class ProductAction extends org.eclipse.equinox.p2.publisher.eclipse.Prod
 
 		IPublisherResult innerResult = new PublisherResult();
 		PublisherInfo innerInfo = new PublisherInfo();
-		innerInfo.setConfigurations(publisherInfo.getConfigurations());
+		String[] configs = publisherInfo.getConfigurations();
+		if(executablesFeatureLocation == null)
+		{
+			// We can only create one single configuration and that's the one of the running platform
+			//
+			Logger log = Buckminster.getLogger();
+			String availableConfig = Platform.getWS() + '.' + Platform.getOS() + '.' + Platform.getOSArch();
+			boolean warningsPrinted = false;
+			boolean availConfigFound = false;
+			for(String config : configs)
+			{
+				if(config.equals(availableConfig))
+				{
+					availConfigFound = true;
+					continue;
+				}
+				log.warning(NLS.bind("Missing executable launcher for configuration {0}", config));
+				warningsPrinted = true;
+			}
+			if(warningsPrinted)
+				log.warning("Perhaps you should install the Platform Executable Launchers feature into your target platform");
+
+			if(!availConfigFound)
+				return Status.OK_STATUS;
+
+			innerInfo.setConfigurations(new String[] { availableConfig });
+			if(Platform.inDevelopmentMode())
+			{
+				String ilProp = System.getProperty("eclipse.host.location"); //$NON-NLS-1$
+				if(ilProp == null)
+				{
+					log.warning("Please use vmarg -Declipse.host.location=${eclipse_home} when running product builds self hosted without the equinox executable feature");
+				}
+				else
+					source = ilProp;
+			}
+			else
+				source = TargetPlatform.getPlatformInstallLocation().getAbsolutePath();
+		}
+		else
+			innerInfo.setConfigurations(configs);
+
 		innerInfo.setArtifactOptions(publisherInfo.getArtifactOptions());
 		innerInfo.setArtifactRepository(publisherInfo.getArtifactRepository());
 		innerInfo.setMetadataRepository(publisherInfo.getMetadataRepository());
@@ -72,6 +119,20 @@ public class ProductAction extends org.eclipse.equinox.p2.publisher.eclipse.Prod
 		if(status.getSeverity() != IStatus.ERROR)
 			results.merge(innerResult, IPublisherResult.MERGE_MATCHING);
 		return status;
+	}
+
+	@Override
+	protected IPublisherAction[] createActions(IPublisherResult results)
+	{
+		IPublisherAction[] actions = super.createActions(results);
+		if(getExecutablesLocation() == null)
+		{
+			IPublisherAction[] newActions = new IPublisherAction[actions.length + 1];
+			System.arraycopy(actions, 0, newActions, 1, actions.length);
+			actions = newActions;
+			actions[0] = createApplicationExecutableAction(info.getConfigurations());
+		}
+		return actions;
 	}
 
 	@Override
