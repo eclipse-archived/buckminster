aa89b1e05780300811b95a35452e170d1e4e511a
Thomas Hallgren <thomas@tada.se>
2009-08-14 08:47:45 +0000

    Bug 280721 - Missing Launcher Icon

--- org.eclipse.buckminster.pde/src/java/org/eclipse/buckminster/pde/tasks/EquinoxExecutableAction.java
+++ org.eclipse.buckminster.pde/src/java/org/eclipse/buckminster/pde/tasks/EquinoxExecutableAction.java
@@ -1,14 +1,49 @@
 package org.eclipse.buckminster.pde.tasks;
 
+import java.io.File;
+
 import org.eclipse.buckminster.core.helpers.TextUtils;
 import org.eclipse.equinox.internal.p2.publisher.eclipse.ExecutablesDescriptor;
+import org.eclipse.equinox.internal.p2.publisher.eclipse.IProductDescriptor;
 import org.eclipse.equinox.internal.provisional.p2.core.Version;
 import org.eclipse.equinox.p2.publisher.eclipse.IBrandingAdvice;
+import org.eclipse.equinox.p2.publisher.eclipse.ProductFileAdvice;
 import org.eclipse.pde.internal.build.BrandingIron;
 
 @SuppressWarnings( { "restriction", "hiding" })
 public class EquinoxExecutableAction extends org.eclipse.equinox.p2.publisher.eclipse.EquinoxExecutableAction
 {
+	// This method is a somewhat ugly fix for bug 280721
+	private static String peruseIcons(IBrandingAdvice advice)
+	{
+		String[] iconsArr = advice.getIcons();
+		if(iconsArr == null || iconsArr.length == 0)
+			return null;
+
+		if(!(advice instanceof ProductFileAdvice))
+			return TextUtils.concat(iconsArr, ","); //$NON-NLS-1$
+
+		IProductDescriptor productFile = ((ProductFileAdvice)advice).getProductFile();
+		File root = productFile.getLocation().getParentFile();
+		if(root != null)
+			root = root.getParentFile();
+		if(root == null)
+			return TextUtils.concat(iconsArr, ","); //$NON-NLS-1$
+
+		int idx = iconsArr.length;
+		while(--idx >= 0)
+		{
+			File iconFile = new File(iconsArr[idx]);
+			if(!iconFile.isFile())
+			{
+				iconFile = new File(root, iconFile.getPath());
+				if(iconFile.isFile())
+					iconsArr[idx] = iconFile.getAbsolutePath();
+			}
+		}
+		return TextUtils.concat(iconsArr, ","); //$NON-NLS-1$
+	}
+
 	public EquinoxExecutableAction(ExecutablesDescriptor executables, String configSpec, String idBase,
 			Version version, String flavor)
 	{
@@ -19,9 +54,10 @@ public class EquinoxExecutableAction extends org.eclipse.equinox.p2.publisher.ec
 	protected void fullBrandExecutables(ExecutablesDescriptor descriptor, IBrandingAdvice advice)
 	{
 		BrandingIron iron = new BrandingIron();
-		String icons = TextUtils.concat(advice.getIcons(), ","); //$NON-NLS-1$
+		String icons = peruseIcons(advice);
 		if(icons != null)
 			iron.setIcons(icons);
+
 		String name = advice.getExecutableName();
 		if(name == null)
 			name = "eclipse"; //$NON-NLS-1$
