ea4bd48b77e32f4e849f2c9631fe267987893c25
Thomas Hallgren <thomas@tada.se>
2009-10-07 17:50:28 +0000

    Old RMAP editor no longer default.

--- org.eclipse.buckminster.subversion/src/org/eclipse/buckminster/subversion/GenericReaderType.java
+++ org.eclipse.buckminster.subversion/src/org/eclipse/buckminster/subversion/GenericReaderType.java
@@ -4,8 +4,10 @@ import java.net.URI;
 import java.util.Date;
 
 import org.eclipse.buckminster.core.RMContext;
+import org.eclipse.buckminster.core.helpers.TextUtils;
 import org.eclipse.buckminster.core.metadata.model.Resolution;
 import org.eclipse.buckminster.core.reader.CatalogReaderType;
+import org.eclipse.buckminster.core.reader.ReferenceInfo;
 import org.eclipse.buckminster.core.version.VersionMatch;
 import org.eclipse.buckminster.core.version.VersionSelector;
 import org.eclipse.buckminster.runtime.BuckminsterException;
@@ -13,9 +15,91 @@ import org.eclipse.buckminster.runtime.MonitorUtils;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.osgi.util.NLS;
 
-public abstract class GenericReaderType<SVN_ENTRY_TYPE,SVN_REVISION_TYPE> extends CatalogReaderType
+public abstract class GenericReaderType<SVN_ENTRY_TYPE, SVN_REVISION_TYPE> extends CatalogReaderType
 {
+
+	@Override
+	public ReferenceInfo extractReferenceInfo(String reference) throws CoreException
+	{
+		String[] parts = TextUtils.split(reference, ",");
+		if(parts.length < 3)
+			throw BuckminsterException.fromMessage(NLS.bind(
+					"The svn readerType cannot parse PSF project reference {0}", reference));
+
+		String repositoryLocation = parts[1];
+		String module = null;
+		VersionSelector selector = null;
+		boolean useScheme = false;
+		int trunkPos = repositoryLocation.indexOf("/trunk");
+		if(trunkPos > 0)
+		{
+			// Do we have something after /trunk ?
+			//
+			int trunkEnd = trunkPos + 6;
+			if(repositoryLocation.length() > trunkEnd)
+			{
+				// We do, then treat that as the module.
+				//
+				module = repositoryLocation.substring(trunkEnd);
+				repositoryLocation = repositoryLocation.substring(0, trunkPos);
+				useScheme = true;
+			}
+		}
+		else
+		{
+			int branchPos = repositoryLocation.indexOf("/branches/");
+			if(branchPos > 0)
+			{
+				// Treat what ever follows directly after /branches/ as a branch name
+				//
+				int branchNameStart = branchPos + 10;
+				int branchNameEnd = repositoryLocation.indexOf('/', branchNameStart);
+				if(branchNameEnd == -1)
+					branchNameEnd = repositoryLocation.length();
+				else
+					module = repositoryLocation.substring(branchNameEnd);
+				selector = VersionSelector.branch(repositoryLocation.substring(branchNameStart, branchNameEnd));
+				repositoryLocation = repositoryLocation.substring(0, branchPos);
+				useScheme = true;
+			}
+			else
+			{
+				int tagPos = repositoryLocation.indexOf("/tags/");
+				if(tagPos > 0)
+				{
+					// Treat what ever follows directly after /tags/ as a tag name
+					//
+					int tagNameStart = tagPos + 6;
+					int tagNameEnd = repositoryLocation.indexOf('/', tagNameStart);
+					if(tagNameEnd == -1)
+						tagNameEnd = repositoryLocation.length();
+					else
+						module = repositoryLocation.substring(tagNameEnd);
+					selector = VersionSelector.tag(repositoryLocation.substring(tagNameStart, tagNameEnd));
+					repositoryLocation = repositoryLocation.substring(0, tagPos);
+					useScheme = true;
+				}
+			}
+		}
+
+		if(useScheme)
+		{
+			StringBuilder thaUri = new StringBuilder();
+			thaUri.append(repositoryLocation);
+			thaUri.append("/trunk");
+			if(module != null)
+			{
+				thaUri.append(module);
+				thaUri.append("?moduleAfterBranch&moduleAfterTag");
+			}
+			else
+				thaUri.append("?moduleBeforeBranch&moduleBeforeTag");
+			repositoryLocation = thaUri.toString();
+		}
+		return new ReferenceInfo(repositoryLocation, selector, parts[2]);
+	}
 
 	final public URI getArtifactURL(Resolution resolution, RMContext context) throws CoreException
 	{
@@ -23,21 +107,13 @@ public abstract class GenericReaderType<SVN_ENTRY_TYPE,SVN_REVISION_TYPE> extend
 		return null;
 	}
 
-	private ISubversionSession<SVN_ENTRY_TYPE,SVN_REVISION_TYPE> getSession(String repositoryURI, VersionSelector branchOrTag)
-			throws CoreException
-	{
-		return getSession(repositoryURI, branchOrTag, -1, null, new RMContext(null));
-	}
-
-	abstract protected ISubversionSession<SVN_ENTRY_TYPE,SVN_REVISION_TYPE> getSession(String repositoryURI, VersionSelector branchOrTag,
-			long revision, Date timestamp, RMContext context) throws CoreException;
-
 	@Override
 	final public Date getLastModification(String repositoryLocation, VersionSelector versionSelector,
 			IProgressMonitor monitor) throws CoreException
 	{
 		monitor.beginTask(null, 1);
-		final ISubversionSession<SVN_ENTRY_TYPE,SVN_REVISION_TYPE> session = getSession(repositoryLocation, versionSelector);
+		final ISubversionSession<SVN_ENTRY_TYPE, SVN_REVISION_TYPE> session = getSession(repositoryLocation,
+				versionSelector);
 		try
 		{
 			return session.getLastTimestamp();
@@ -55,7 +131,8 @@ public abstract class GenericReaderType<SVN_ENTRY_TYPE,SVN_REVISION_TYPE> extend
 			IProgressMonitor monitor) throws CoreException
 	{
 		monitor.beginTask(null, 1);
-		final ISubversionSession<SVN_ENTRY_TYPE,SVN_REVISION_TYPE> session = getSession(repositoryLocation, versionSelector);
+		final ISubversionSession<SVN_ENTRY_TYPE, SVN_REVISION_TYPE> session = getSession(repositoryLocation,
+				versionSelector);
 		try
 		{
 			return session.getLastChangeNumber();
@@ -73,11 +150,10 @@ public abstract class GenericReaderType<SVN_ENTRY_TYPE,SVN_REVISION_TYPE> extend
 			throws CoreException
 	{
 		VersionMatch vm = cr.getVersionMatch();
-		ISubversionSession<SVN_ENTRY_TYPE,SVN_REVISION_TYPE> session = getSession(cr.getRepository(), vm.getBranchOrTag(), vm
-				.getRevision(), vm.getTimestamp(), context);
+		ISubversionSession<SVN_ENTRY_TYPE, SVN_REVISION_TYPE> session = getSession(cr.getRepository(),
+				vm.getBranchOrTag(), vm.getRevision(), vm.getTimestamp(), context);
 		session.createCommonRoots(context);
-		session = getSession(cr.getRepository(), vm.getBranchOrTag(), vm
-				.getRevision(), vm.getTimestamp(), context);
+		session = getSession(cr.getRepository(), vm.getBranchOrTag(), vm.getRevision(), vm.getTimestamp(), context);
 		try
 		{
 			updateRepositoryMap(project, session);
@@ -93,6 +169,15 @@ public abstract class GenericReaderType<SVN_ENTRY_TYPE,SVN_REVISION_TYPE> extend
 		MonitorUtils.complete(monitor);
 	}
 
-	abstract protected void updateRepositoryMap(IProject project, ISubversionSession<SVN_ENTRY_TYPE,SVN_REVISION_TYPE> session)
-			throws Exception;
+	abstract protected ISubversionSession<SVN_ENTRY_TYPE, SVN_REVISION_TYPE> getSession(String repositoryURI,
+			VersionSelector branchOrTag, long revision, Date timestamp, RMContext context) throws CoreException;
+
+	abstract protected void updateRepositoryMap(IProject project,
+			ISubversionSession<SVN_ENTRY_TYPE, SVN_REVISION_TYPE> session) throws Exception;
+
+	private ISubversionSession<SVN_ENTRY_TYPE, SVN_REVISION_TYPE> getSession(String repositoryURI,
+			VersionSelector branchOrTag) throws CoreException
+	{
+		return getSession(repositoryURI, branchOrTag, -1, null, new RMContext(null));
+	}
 }
