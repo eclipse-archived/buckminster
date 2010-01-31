23b0207199d9486cec846b0a472d07b253e31cd0
Thomas Hallgren <thomas@tada.se>
2010-01-31 09:28:13 +0000

    Bug 216413 - Deprecate the eclipse.import reader type

--- org.eclipse.buckminster.pde/src/java/org/eclipse/buckminster/pde/internal/EclipseBundleType.java
+++ org.eclipse.buckminster.pde/src/java/org/eclipse/buckminster/pde/internal/EclipseBundleType.java
@@ -9,12 +9,20 @@
 package org.eclipse.buckminster.pde.internal;
 
 import org.eclipse.buckminster.core.CorePlugin;
+import org.eclipse.buckminster.core.cspec.builder.CSpecBuilder;
 import org.eclipse.buckminster.core.ctype.AbstractComponentType;
 import org.eclipse.buckminster.core.ctype.IResolutionBuilder;
+import org.eclipse.buckminster.core.metadata.model.BOMNode;
 import org.eclipse.buckminster.core.reader.IComponentReader;
+import org.eclipse.buckminster.core.reader.IReaderType;
+import org.eclipse.buckminster.core.reader.P2ReaderType;
+import org.eclipse.buckminster.core.version.ProviderMatch;
+import org.eclipse.buckminster.pde.cspecgen.PDEBuilder;
 import org.eclipse.buckminster.runtime.MonitorUtils;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 
 /**
  * @author Thomas Hallgren
@@ -26,5 +34,29 @@ public class EclipseBundleType extends AbstractComponentType
 	{
 		MonitorUtils.complete(monitor);
 		return CorePlugin.getDefault().getResolutionBuilder(IResolutionBuilder.PLUGIN2CSPEC);
+	}
+
+	@Override
+	protected BOMNode getResolution(ProviderMatch rInfo, boolean forResolutionAidOnly, IProgressMonitor monitor)
+			throws CoreException
+	{
+		try
+		{
+			SubMonitor subMon = SubMonitor.convert(monitor, 20);
+			IReaderType readerType = rInfo.getReaderType();
+			if(readerType instanceof EclipseImportReaderType)
+			{
+				IInstallableUnit iu = P2ReaderType.getIU(rInfo, subMon.newChild(10));
+				if(iu != null)
+					return PDEBuilder.createNode(rInfo, new CSpecBuilder(P2ReaderType.getMetadataRepository(rInfo,
+							subMon.newChild(10)), iu));
+			}
+			return super.getResolution(rInfo, forResolutionAidOnly, subMon.newChild(10));
+		}
+		finally
+		{
+			if(monitor != null)
+				monitor.done();
+		}
 	}
 }
