22d6fc392171b228515c9ead655c26bb3e633990
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2010-02-18 15:39:59 +0000

    Removing moved b3 projects from buckminster trunk

--- org.eclipse.b3.beelang/src/org/eclipse/b3/RegularExpression.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.b3;
-
-public class RegularExpression {
-	private String expression;
-	public RegularExpression(String x) {
-		expression = x; 
-		}
-	public String toString() { return expression; }
-}
