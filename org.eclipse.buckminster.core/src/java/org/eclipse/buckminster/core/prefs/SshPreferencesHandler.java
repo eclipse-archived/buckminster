269e8684fad5768dd09798eed50e843e83e0a5fc
Michal Ruzicka <michal.ruzicka@cloudsmith.com>
2010-05-27 01:10:41 +0000

    Bug 310204 -  provide SSH configuration preferences

---
+++ org.eclipse.buckminster.core/src/java/org/eclipse/buckminster/core/prefs/SshPreferencesHandler.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2010, Cloudsmith Inc.
+ * The code, documentation and other materials contained herein have been
+ * licensed under the Eclipse Public License - v 1.0 by the copyright holder
+ * listed above, as the Initial Contributor under such license. The text of
+ * such license is available at www.eclipse.org.
+ *******************************************************************************/
+
+package org.eclipse.buckminster.core.prefs;
+
+import org.eclipse.buckminster.cmdline.BasicPreferenceHandler;
+import org.eclipse.jsch.internal.core.IConstants;
+import org.eclipse.jsch.internal.core.JSchCorePlugin;
+import org.osgi.service.prefs.BackingStoreException;
+
+/**
+ * Preference handler class to ensure that updates of the ssh preferences are
+ * honored by the ssh code.
+ * 
+ * @author michal.ruzicka@cloudsmith.com
+ */
+@SuppressWarnings("restriction")
+public class SshPreferencesHandler extends BasicPreferenceHandler {
+
+	@Override
+	public void set(String privateKeys) throws BackingStoreException {
+		super.set(privateKeys);
+
+		String key = pathAsNodeAndKey(getKey())[1];
+
+		// make sure the ssh code picks up the updated preference value
+		// when it is needed
+		if (IConstants.KEY_PRIVATEKEY.equals(key)) {
+			JSchCorePlugin.getPlugin().setNeedToLoadKeys(true);
+		} else if (IConstants.KEY_SSH2HOME.equals(key)) {
+			JSchCorePlugin.getPlugin().setNeedToLoadKnownHosts(true);
+		}
+	}
+
+}
