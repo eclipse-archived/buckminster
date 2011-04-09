7f78724872bb36f4bfe42ceed869190ff4eee929
thallgren <thallgren@ee007c2a-0a25-0410-9ab9-bf268980928c>
2011-04-09 14:34:10 +0000

    Bug 342354 - PDEMapProvider needs a way to do token substitution in the map files.

--- org.eclipse.buckminster.pde.test/src/org/eclipse/buckminster/pde/test/MapFileTest.java
+++ org.eclipse.buckminster.pde.test/src/org/eclipse/buckminster/pde/test/MapFileTest.java
@@ -23,30 +23,26 @@ import org.eclipse.buckminster.runtime.Logger;
  * @author Thomas Hallgren
  * 
  */
-public class MapFileTest extends TestCase
-{
+public class MapFileTest extends TestCase {
 	@Override
-	public void setUp() throws Exception
-	{
+	public void setUp() throws Exception {
 		BuckminsterPreferences.setLogLevelConsole(Logger.DEBUG);
 		BuckminsterPreferences.setLogLevelEclipseLogger(Logger.SILENT);
 	}
 
-	public void testMap() throws Exception
-	{
+	public void testMap() throws Exception {
 		ArrayList<MapFileEntry> bld = new ArrayList<MapFileEntry>();
 		URL url = new URL("http://download.eclipse.org/tools/orbit/downloads/drops/R20100114021427/directory.txt"); //$NON-NLS-1$
 		InputStream input = url.openStream();
-		MapFile.parse(input, url.toString(), bld);
+		MapFile.parse(input, url.toString(), null, null, bld);
 		input.close();
 
-		url = new URL(
-				"http://download.eclipse.org/tools/orbit/downloads/drops/R20100114021427/orbitBundles-R20100114021427.map"); //$NON-NLS-1$
+		url = new URL("http://download.eclipse.org/tools/orbit/downloads/drops/R20100114021427/orbitBundles-R20100114021427.map"); //$NON-NLS-1$
 		input = url.openStream();
-		MapFile.parse(input, url.toString(), bld);
+		MapFile.parse(input, url.toString(), null, null, bld);
 		input.close();
 
-		for(MapFileEntry entry : bld)
+		for (MapFileEntry entry : bld)
 			System.out.println(entry);
 	}
 }
